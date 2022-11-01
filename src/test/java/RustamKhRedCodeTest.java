190f50827ce4fbb33aa6814dd6a9c222ac8e4930
RustamKhudoyarov <khrust_lord@mail.ru>
2022-11-01 18:03:30 -0500

    Add test methods testLinkName(),  testLinkOpen()

--- src/test/java/RustamKhRedCodeTest.java
+++ src/test/java/RustamKhRedCodeTest.java
@@ -2,9 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+
+import java.time.Duration;
 
 public class RustamKhRedCodeTest extends BaseTest {
     @Test
@@ -16,6 +20,19 @@ public class RustamKhRedCodeTest extends BaseTest {
         String Link =  getDriver().findElement(By.xpath("//li/a[text()='Autocomplete']")).getText();
         Assert.assertEquals(Link, "Autocomplete");
         
+
+    }
+    @Test
+    public void testLinkOpen() throws InterruptedException{
+
+
+        getDriver().get("https://formy-project.herokuapp.com/");
+        getDriver().findElement(By.xpath("//li/a[text()='Autocomplete']")).click();
+        Thread.sleep(2000);
+        String actualResult =  getDriver().findElement(By.xpath("//h1")).getText();
+
+        Assert.assertEquals(actualResult, "Autocomplete");
+
 
     }
 }
