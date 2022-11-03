9c41dcf0f7ce5cf5cbd1477fafe96f7b4803dd80
RustamKhudoyarov <khrust_lord@mail.ru>
2022-11-03 08:36:17 -0500

    Add test methods testAutoCompleteFieldsWebForm() : actions- autofill fields, dropdown menu, pushing button

--- src/test/java/RustamKhRedCodeTest.java
+++ src/test/java/RustamKhRedCodeTest.java
@@ -1,16 +1,12 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-import java.time.Duration;
 
 public class RustamKhRedCodeTest extends BaseTest {
+    private String baseURL = "https://formy-project.herokuapp.com";
     @Test
 
     public void testLinkName(){
@@ -23,15 +19,35 @@ public class RustamKhRedCodeTest extends BaseTest {
 
     }
     @Test
-    public void testLinkOpen() throws InterruptedException{
+    public void testAutoCompleteFieldsWebForm() {
 
 
-        getDriver().get("https://formy-project.herokuapp.com/");
-        getDriver().findElement(By.xpath("//li/a[text()='Autocomplete']")).click();
-        Thread.sleep(2000);
-        String actualResult =  getDriver().findElement(By.xpath("//h1")).getText();
+        getDriver().get(baseURL + "/form");
+        WebElement firstName = getDriver().findElement(By.xpath("//input[@id ='first-name']"));
+        firstName.sendKeys("Don");
+        WebElement lastName = getDriver().findElement(By.xpath("//input[@id ='last-name']"));
+        lastName.sendKeys("Red");
+        WebElement jobTitle = getDriver().findElement(By.xpath("//input[@id ='job-title']"));
+        jobTitle.sendKeys("QA");
+        WebElement educationCollege = getDriver().findElement(By.xpath("//div/input[@id='radio-button-2']"));
+        educationCollege.click();
+        WebElement selectSexMale = getDriver()
+                .findElement(By
+                        .xpath("//div[@class ='input-group']/div/input[@type='checkbox' and @value='checkbox-1']"));
+        selectSexMale.click();
+         getDriver().findElement(By.xpath("//select[@id='select-menu']")).click();
+        WebElement selectExperience =getDriver().findElement(By.xpath("//select/option[@value ='2']"));
+        selectExperience.click();
+        getDriver().findElement(By.xpath("//input[@id='datepicker']")).click();
+        WebElement dateToday = getDriver().findElement(By.xpath("//div/div/table/tbody/tr/td[@class='today day']"));
+        dateToday.click();
+        WebElement submitButton = getDriver().findElement(By.xpath("//a[@href='/thanks']"));
+        submitButton.click();
+        String actualResult = getDriver().findElement(By.xpath("//div[@class='alert alert-success']")).getText();
+        Assert.assertEquals(actualResult, "The form was successfully submitted!");
 
-        Assert.assertEquals(actualResult, "Autocomplete");
+
+
 
 
     }
