48203058fa6634c142a225604ea8f24305ca3237
Ilya Zozulia <89859127+IlyaZozulia@users.noreply.github.com>
2022-11-04 22:24:09 +0300

    add second test (#172)
    * add second test
    
    * Update JavanistyTest.java
    
    * Update JavanistyTest.java
    
    * Update JavanistyTest.java

--- src/test/java/JavanistyTest.java
+++ src/test/java/JavanistyTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -41,7 +42,8 @@ public class JavanistyTest extends BaseTest {
 
         Assert.assertEquals(expectedConfirmationMessage, actualConfirmationMessage);
     }
-
+     
+    @Ignore
     @Test
     public void testBdoWarrior2() throws InterruptedException {
         getDriver().get("https://bdocodex.com/ru/skillbuilder/");
@@ -87,6 +89,7 @@ public class JavanistyTest extends BaseTest {
         Assert.assertTrue(getDriver().findElement(By.xpath("//input[@class='content']")).isDisplayed());
     }
     
+    @Ignore
     @Test
     public void testTextHlebnica(){
         getDriver().get("http://hlebnitca.ru/");
@@ -94,10 +97,18 @@ public class JavanistyTest extends BaseTest {
         Assert.assertEquals(getDriver().getCurrentUrl(), "http://hlebnitca.ru/about");
     }
 
+    @Ignore
     @Test
     public void testAboutHlebnica(){
         getDriver().get ("http://hlebnitca.ru/about");
         String aboutHlebnica =  getDriver().findElement(By.xpath("//div[@class = 't396__elem tn-elem tn-elem__3963063211640603855210']")).getText();
         Assert.assertEquals(aboutHlebnica, "Вкус настоящей домашней выпечки");
+    }
+    
+    @Test
+    public void testIlyaSecondTest() {
+        getDriver().get("https://karkas.k3-cottage.ru/");
+        WebElement text = getDriver().findElement(By.xpath("//li/a[@href='#features']"));
+        Assert.assertEquals(text.getText(), "ВОЗМОЖНОСТИ");
     }
 }
