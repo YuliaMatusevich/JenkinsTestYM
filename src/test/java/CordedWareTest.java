a2a15732641b905e53a1bf965df0c8feea11c6ef
Ivan Tretyakov <ivtretyakov56@gmail.com>
2022-11-05 03:38:25 +0500

    First test 05.11.22

---
+++ src/test/java/CordedWareTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class CordedWareTest extends BaseTest {
+
+    @Test
+    public void testHerokuApp() {
+        getDriver().get("https:/formy-project.herokuapp.com/");
+
+        WebElement link = getDriver().findElement(
+                By.xpath("//li/a[@href='/autocomplete']")
+        );
+        Assert.assertEquals(link.getText(), "Autocomplete");
+    }
+}
