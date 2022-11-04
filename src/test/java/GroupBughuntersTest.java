f1b137f7c75aaf4410f4f80a34b3683df4e8910a
Ervin Iliasov <eiliasov@yahoo.com>
2022-11-04 16:12:52 -0400

    май ферст тест

--- src/test/java/GroupBughuntersTest.java
+++ src/test/java/GroupBughuntersTest.java
@@ -53,7 +53,6 @@ public class GroupBughuntersTest extends BaseTest {
         getDriver().get("https://www.w3resource.com/index.php");
 
         WebElement link = getDriver().findElement(By.xpath("//a[@href='https://www.w3resource.com/java-tutorial/index.php']"));
-
         Assert.assertEquals(link.getText(), "Java");
     }
 }
