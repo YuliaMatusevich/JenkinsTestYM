42d011bb4e007b2b34543dded5fc58dfd34b1ac6
ServachakM <102581167+ServachakM@users.noreply.github.com>
2022-11-05 10:25:26 +0200

    New tests by ServachakM added (#186)
    * New test by ServachakM added

--- src/test/java/CleanCodeTest.java
+++ src/test/java/CleanCodeTest.java
@@ -1,9 +1,12 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+
+import java.time.Duration;
 
 public class CleanCodeTest extends BaseTest {
 
@@ -57,7 +60,8 @@ public class CleanCodeTest extends BaseTest {
 
         WebElement link = getDriver().findElement(By.xpath("//li/a[@href='/dropdown']"));
 
-        Assert.assertEquals(link.getText(), "Dropdown"); }
+        Assert.assertEquals(link.getText(), "Dropdown"); 
+    }
 
     @Test
     public void testTextContactsIsPresent() {
@@ -66,7 +70,6 @@ public class CleanCodeTest extends BaseTest {
         WebElement text = getDriver().findElement(By.xpath("//span[text()='КОНТАКТЫ']"));
 
         Assert.assertEquals(text.getText(), "КОНТАКТЫ");
-
     }
 
     @Test
@@ -75,18 +78,48 @@ public class CleanCodeTest extends BaseTest {
         WebElement link = getDriver().findElement(By.xpath("//li/a[@href='/buttons']"));
         Assert.assertEquals(link.getText(),"Buttons");
     }
-
-
-
-        @Test
+    
+       @Test
         public void testAstraCom() {
             getDriver().get("https://www.astracom.ru/");
             getDriver().findElement(By.className("button")).click();
             getDriver().findElement(By.partialLinkText("АРСО P25")).click();
             WebElement text = getDriver().findElement(By.partialLinkText("Ретрансляторы"));
             Assert.assertEquals(text.getText(), "Ретрансляторы");
-
         }
 
+    @Test
+    public void testBox24Language() {
+        final String URL = "https://box24.com.ua/";
+        String expectedLanguage = "Укр";
 
+        getDriver().get(URL);
+        String actualLanguage = getDriver().findElement(
+                By.xpath("//div[@class='lang-menu__button']")).getText();
+
+        Assert.assertEquals(actualLanguage, expectedLanguage);
+    }
+
+    @Test
+    public void testBox24SearchForm() {
+        final String URL = "https://box24.com.ua/";
+
+        String expectedH1 = "Результати пошуку «plane»";
+        String input = "plane";
+
+        getDriver().get(URL);
+        getDriver().findElement(By.cssSelector(".search__input")).sendKeys(input);
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector(".search__button")));
+
+        getDriver().findElement(By.cssSelector(".search__button")).click();
+
+        String actualH1 = getDriver().findElement(
+                By.cssSelector("#j-catalog-header")).getText();
+
+        Assert.assertEquals(actualH1, expectedH1);
+    }
 }
