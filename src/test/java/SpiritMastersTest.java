9e24a8e1a2527a845d69788fa2480e345d1c05af
anastasia-kaz <anastasia-kaz@mail.ru>
2022-11-05 13:36:13 +0300

    add first test anastasia-kaz

--- src/test/java/SpiritMastersTest.java
+++ src/test/java/SpiritMastersTest.java
@@ -439,4 +439,31 @@ public class SpiritMastersTest extends BaseTest {
         Assert.assertEquals(currentUrl, "https://openweathermap.org/guide");
         Assert.assertEquals(currentTitle, "OpenWeatherMap API guide - OpenWeatherMap");
     }
+
+    @Test
+    public void testCheckValueCart_AKaz() {
+        getDriver().get("http://saucedemo.com/");
+
+        String login = "standard_user";
+        String pass = "secret_sauce";
+
+        WebElement loginInput = getDriver().findElement(By.id("user-name"));
+        loginInput.sendKeys(login);
+
+        WebElement passwordInput = getDriver().findElement(By.id("password"));
+        passwordInput.sendKeys(pass);
+
+        WebElement loginBtn = getDriver().findElement(By.id("login-button"));
+        loginBtn.click();
+
+        List<WebElement> productList = getDriver().findElements(By.cssSelector(".inventory_item"));
+        for (int i = 1; i <= productList.size(); i++) {
+            String path = "//div[@class='inventory_item'][" + i + "]//button";
+            getDriver().findElement(By.xpath(path)).click();
+        }
+
+        WebElement actualResult = getDriver().findElement(By.cssSelector("span.shopping_cart_badge"));
+
+        Assert.assertEquals(actualResult.getText(), "6");
+    }
 }
