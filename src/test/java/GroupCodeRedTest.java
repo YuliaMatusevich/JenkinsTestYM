b4ef0770bd1b340c2bf1cd73a5dd69e1ebe0360e
Viktoriya D <115054456+ViktoriyaED@users.noreply.github.com>
2022-11-04 23:38:22 -0400

    adding Openweathermap test to CodeRed (#194)
    * adding Openweathermap test to CodeRed
    
    * Correcting an error

--- src/test/java/GroupCodeRedTest.java
+++ src/test/java/GroupCodeRedTest.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 import java.time.Duration;
+import java.util.List;
 
 
 public class GroupCodeRedTest extends BaseTest {
@@ -266,5 +268,25 @@ public class GroupCodeRedTest extends BaseTest {
 
         Assert.assertEquals(actualResult_1, expectedResult_1);
         Assert.assertEquals(actualResult_2, expectedResult_2);
+    }
+
+   @Test
+    public void testAPICheck30Buttons() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+        int expectedResult = 30;
+
+        getDriver().get(url);
+        getDriver().manage().window().maximize();
+        Thread.sleep(4000);
+
+        getDriver().findElement(By.xpath("//div[@id='desktop-menu']//a[@href='/api']")).click();
+
+        List<WebElement> orangeButtons29 = getDriver().findElements(By.xpath("//a[@class='btn_block orange round']"));
+        List<WebElement> orangeButtons1 = getDriver().findElements(By.xpath("//a[@class='ow-btn round btn-orange']"));
+
+        int actualResult = orangeButtons29.size() + orangeButtons1.size();
+
+        Assert.assertEquals(actualResult, expectedResult);
     }
 }
