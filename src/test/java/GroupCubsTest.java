32d5ccdfd841106ac9e5ce0937f0cc11d71821d4
Elizaveta Skobeleva <115572773+ElizavetaSkobeleva@users.noreply.github.com>
2022-11-06 17:35:25 +0300

    add test (#235)

--- src/test/java/GroupCubsTest.java
+++ src/test/java/GroupCubsTest.java
@@ -8,12 +8,13 @@ import runner.BaseTest;
 import java.util.Locale;
 
 public class GroupCubsTest extends BaseTest {
+
     @Ignore
     @Test
     public void testFelix_IX() {
         getDriver().get("https://habr.com/ru/all/");
 
-        String query ="приоритет тест-кейса в TestNG";
+        String query = "приоритет тест-кейса в TestNG";
         getDriver().findElement(By.xpath("//a[@data-test-id='search-button']")).click();
         getDriver().findElement(By.className("tm-input-text-decorated__input")).sendKeys(query + "\n");
         getDriver().findElement(By.xpath("//article[@id='588466']/div[1]/h2")).click();
@@ -22,7 +23,7 @@ public class GroupCubsTest extends BaseTest {
     }
 
     @Test
-    public void testRp5(){
+    public void testRp5() {
         getDriver().get("https://rp5.ru");
         WebElement search = getDriver().findElement(By.name("searchStr"));
         search.sendKeys("Танжер\n");
@@ -31,7 +32,7 @@ public class GroupCubsTest extends BaseTest {
     }
 
     @Test
-    public void testAnastasiiaApp(){
+    public void testAnastasiiaApp() {
         getDriver().get("https://koma.lux.pl/");
         WebElement link = getDriver().findElement(By.xpath("//a[@href='https://koma.lux.pl/Wszystkie-produkty,pid,9.html']"));
         Assert.assertEquals(link.getText(), "Wyszukiwanie zaawansowane");
@@ -56,5 +57,28 @@ public class GroupCubsTest extends BaseTest {
         WebElement searchResult = getDriver().findElement(By.xpath("//ul[@class = 'product_list grid row']/li[1]/div/div/h5/a"));
         Assert.assertTrue(searchResult.getText().toLowerCase(Locale.ROOT).contains(query));
     }
+
+    @Test
+    public void testAsh() {
+        getDriver().get("https://www.saucedemo.com/");
+
+        WebElement text = getDriver().findElement(By.id("user-name"));
+        text.sendKeys("standard_user");
+        WebElement text2 = getDriver().findElement(By.id("password"));
+        text2.sendKeys("secret_sauce");
+        WebElement link = getDriver().findElement(By.id("login-button"));
+        getDriver().findElement(By.id("login-button")).click();
+        getDriver().findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        String link2 = getDriver().findElement(By.xpath("//*[contains(@name, 'remove-sauce-labs-backpack')]")).getText();
+        Assert.assertEquals(link2, "REMOVE");
+    }
+     @Test
+     public void testLiza() {
+        getDriver().get("https://petstore.octoperf.com/actions/Catalog.action");
+        WebElement link = getDriver().findElement(By.xpath("//*[@id=\"MenuContent\"]/a[3]"));
+        Assert.assertEquals(link.getText(), "?");
+    }
 }
+
+
 
