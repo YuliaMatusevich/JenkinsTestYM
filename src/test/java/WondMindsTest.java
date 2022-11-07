391591941e58508901f042b57d93a489890939b3
TaniaKuno <tania.kuno@gmail.com>
2022-11-07 10:53:13 -0800

    add test TaniaKuno (#267)

--- src/test/java/WondMindsTest.java
+++ src/test/java/WondMindsTest.java
@@ -9,11 +9,27 @@ import runner.BaseTest;
 public class WondMindsTest extends BaseTest {
 
     @Test
-    public void testGorodTulaTheBest(){
+    public void testGorodTulaTheBest() {
         getDriver().get("https://rp5.ru");
         WebElement search = getDriver().findElement(By.name("searchStr"));
         search.sendKeys("Тула\n");
         String actualText = getDriver().findElement(By.xpath("//h1")).getText();
         Assert.assertEquals(actualText, "Search result");
+    }
+
+    @Test
+    public void testAmazingBouqets() {
+
+        String url = "https://paeonia-boutique.ca/";
+        String expectedResult = "Paeonia Fleuristerie Boutique";
+
+        getDriver().get(url);
+
+        WebElement link = getDriver().findElement(By.xpath("//span[text() = "
+                + "'Paeonia Fleuristerie Boutique']"));
+
+        String actualResult = link.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
     }
 }
