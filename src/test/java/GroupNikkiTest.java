fa511cf20dcb619c3a3ebcc8b2e7bdce6e71bd01
Arailym <33895096+ArailymAsar@users.noreply.github.com>
2022-11-07 07:04:04 +0600

    isDisplayed (#241)
    * isDisplayed
    
    * isDisplayed

--- src/test/java/GroupNikkiTest.java
+++ src/test/java/GroupNikkiTest.java
@@ -85,7 +85,16 @@ public class GroupNikkiTest extends BaseTest {
         }
     }
 
-
+    @Test
+    public void testArailymIsElementsDisplayed(){
+        getDriver().get("https://katalon-demo-cura.herokuapp.com/");
+        Assert.assertTrue(getDriver().findElement(By.xpath("//i[@class='fa fa-facebook fa-fw fa-3x']"))
+                .isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.xpath("//i[@class='fa fa-twitter fa-fw fa-3x']"))
+                .isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.xpath("//i[@class='fa fa-dribbble fa-fw fa-3x']"))
+                .isDisplayed());
+    }
     }
 
 
