205c94613f840453f0d264a70ede63cd8f40fd06
Liudmila Plucci <112190395+LiudmilaPlucci@users.noreply.github.com>
2022-11-08 21:18:58 +0000

    GroupSpiritMastersTest | testBankRegisterLogin_MW() (#298)
    * made changes to testBankRegisterLogin_MW
    
    * Happy strawberry/at (#294)
    
    * New test and refactor
    
    * Deleted empty string (#299)
    
    * MyFirstCommit(added test Kirill Shumakov) (#295)
    
    * MyFirstCommit(added test Kirill Shumakov)
    
    * Ignor tests: testBbcChangeLanguage(GroupBughuntersTest), testEmail(GroupCodeRedTest)
    
    * Refactored test zyzBankRegisterLogin_MW_Test (#303)
    
    * made changes to testBankRegisterLogin_MW
    
    Co-authored-by: Liudmila Plucci <112190395+liuda33@users.noreply.github.com>
    Co-authored-by: Happy Strawberry <85979179+Iriskus@users.noreply.github.com>
    Co-authored-by: Anna Fedorova <snysmymric@gmail.com>
    Co-authored-by: Kirill Shumakov <87996853+kirillshuma@users.noreply.github.com>

--- src/test/java/GroupSpiritMastersTest.java
+++ src/test/java/GroupSpiritMastersTest.java
@@ -231,7 +231,7 @@ public class GroupSpiritMastersTest extends BaseTest {
     }
 
     @Test
-    public void zyzBankRegisterLogin_MW_Test() {
+    public void testBankRegisterLogin_MW() {
         getDriver().get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
 
         getDriver().findElement(By.xpath("//button[normalize-space()='Bank Manager Login']")).click();
@@ -248,22 +248,22 @@ public class GroupSpiritMastersTest extends BaseTest {
         postcode.sendKeys("12334");
 
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+
         Alert confAllert = getDriver().switchTo().alert();
+        String userId = confAllert.getText().substring(confAllert.getText().indexOf(":") + 1);
         confAllert.accept();
 
         getDriver().findElement(By.xpath("//button[@class='btn btn-lg tab btn-primary']")).click();
-        getDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/button[1]")).click();
+        getDriver().findElement(By.cssSelector(".btn.home")).click();
         WebElement login = getDriver().findElement(By.xpath("//button[normalize-space()='Customer Login']"));
-
-        Assert.assertEquals(login.getText(), "Customer Login");
-
         login.click();
         WebElement selectNameVariant = getDriver().findElement(By.id("userSelect"));
         Select dropdown = new Select(selectNameVariant);
-        dropdown.selectByValue("6");
+        dropdown.selectByValue(userId);
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/strong/span")).getText(), "John NeJonh");
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//strong/span[@class='fontBig ng-binding']")).getText(), "John NeJonh");
     }
 
     @Test
