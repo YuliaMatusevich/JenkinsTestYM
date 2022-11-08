0151b1db74ee6c5924ca81d6468bedb1fabce171
foxtiptop <imonthismail@gmail.com>
2022-11-07 19:02:03 -0800

    foxtiptop slider test (#278)
    * Created Group and Added First Test
    
    * Created Group and Added First Test
    
    * Created Group and Added First Test
    
    * Created Group and Added First Test

--- src/test/java/GroupDreamTeamTest.java
+++ src/test/java/GroupDreamTeamTest.java
@@ -1,5 +1,5 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -42,5 +42,31 @@ public class GroupDreamTeamTest extends BaseTest {
         sel.selectByValue("1");
         WebElement one = getDriver().findElement(By.xpath("//option[@value='1']"));
         Assert.assertTrue(one.isSelected());
+    }
+
+    @Test
+    public void testFoxtiptopSliderMouse() {
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement exampleRange = getDriver().findElement(By.cssSelector("body main  div form div div:nth-child(3) label:nth-child(3) input"));
+        Assert.assertTrue(exampleRange.isDisplayed());
+
+        Actions slide = new Actions(getDriver());
+        slide.moveToElement(exampleRange).clickAndHold(exampleRange).moveByOffset(50, 0)
+                .release().build().perform();
+
+        int rangeValue = Integer.parseInt(exampleRange.getAttribute("value"));
+        Assert.assertEquals(rangeValue, 6);
+    }
+
+    @Test
+    public void testFoxtiptopSliderKeyboard() {
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement exampleRange = getDriver().findElement(By.name("my-range"));
+        for (int i = 1; i <= 4; i++) {
+            exampleRange.sendKeys(Keys.ARROW_RIGHT);
+        }
+
+        int rangeValue = Integer.parseInt(exampleRange.getAttribute("value"));
+        Assert.assertEquals(rangeValue, 9);
     }
 }
