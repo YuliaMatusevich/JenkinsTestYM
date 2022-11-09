a1c166efb6f60c45d51bfea019d194c3789a4560
Валентин Куприн (xbrookx) <72659405+ValentinKuprin@users.noreply.github.com>
2022-11-09 07:04:28 +0300

    get text from pop up (#300)

--- src/test/java/GroupJavaStartTest.java
+++ src/test/java/GroupJavaStartTest.java
@@ -6,13 +6,15 @@ import runner.BaseTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class GroupJavaStartTest extends BaseTest {
 
-    @Test
-    public void testHerokuApp() {
+    private final String FARM_PROJECT_URL = "https://formy-project.herokuapp.com/";
 
-        getDriver().get("https://formy-project.herokuapp.com/");
+    @Test
+    public void tes_tHerokuApp() {
+        getDriver().get(FARM_PROJECT_URL);
 
         WebElement link = getDriver().findElement(By.xpath("//li/a[@href='/autocomplete']"));
 
@@ -20,8 +22,8 @@ public class GroupJavaStartTest extends BaseTest {
     }
 
     @Test
-    public void testHerokuAppSearchCheckboxMenu() {
-        getDriver().get("https://formy-project.herokuapp.com/");
+    public void test_HerokuAppSearchCheckboxMenu() {
+        getDriver().get(FARM_PROJECT_URL);
 
         //Thread.sleep(5000);
         WebElement link1 = getDriver().findElement(By.xpath("//li/a[@href='/checkbox']"));
@@ -29,10 +31,9 @@ public class GroupJavaStartTest extends BaseTest {
         Assert.assertEquals(link1.getText(), "Checkbox");
     }
 
-
     @Test
-    public void testHerokuAppComponentsList(){
-        getDriver().get("https://formy-project.herokuapp.com/");
+    public void test_HerokuAppComponentsList(){
+        getDriver().get(FARM_PROJECT_URL);
 
         WebElement buttons = getDriver().findElement(By.xpath("//li/a[text()='Buttons']"));
 
@@ -40,9 +41,8 @@ public class GroupJavaStartTest extends BaseTest {
     }
 
     @Test
-    public void testHerokuApp_QuantityOfDropDownMenuLinks(){
-
-        getDriver().get("https://formy-project.herokuapp.com/");
+    public void test_HerokuApp_QuantityOfDropDownMenuLinks(){
+        getDriver().get(FARM_PROJECT_URL);
 
         int expectedResult = 14;
 
@@ -63,4 +63,31 @@ public class GroupJavaStartTest extends BaseTest {
         Assert.assertEquals(actualResult, expectedResult);
     }
 
+    @Test
+    public void test_GoToPopUpWindow() {
+        getDriver().get("http://omayo.blogspot.com/");
+
+        String mainWindow = getDriver().getWindowHandle();
+
+        getDriver().findElement(
+                By.xpath("//a[text()= 'Open a popup window']")).click();
+
+        Set<String> windows = getDriver().getWindowHandles();
+        String popUpWindow = "";
+
+        for (String changeWindow : windows) {
+            if (!mainWindow.equals(changeWindow)) {
+                popUpWindow = changeWindow;
+                break;
+            }
+        }
+
+        getDriver().switchTo().window(popUpWindow);
+
+        String text = getDriver().findElement(By.id("para1")).getText();
+        String text2 = getDriver().findElement(By.id("para2")).getText();
+
+        Assert.assertEquals(text, "A paragraph of text");
+        Assert.assertEquals(text2, "Another paragraph of text");
+    }
 }
