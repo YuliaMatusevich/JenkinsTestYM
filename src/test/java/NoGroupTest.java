0e58a36129bdc2657e89e1db9b31bda1039a6f7e
Roman T <93357374+Rom-T@users.noreply.github.com>
2022-11-09 14:24:43 -0500

    Refactoring testVerifyTextBoxOutputResult() (#331)
    Refactoring test

--- src/test/java/NoGroupTest.java
+++ src/test/java/NoGroupTest.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -11,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NoGroupTest extends BaseTest {
-@Ignore
+
     @Test
     public void testVerifyTextBoxOutputResult() {
         getDriver().get("https://demoqa.com/");
@@ -27,16 +26,18 @@ public class NoGroupTest extends BaseTest {
         expectedResult.add(String.format("Current Address :%s", currentAdr));
         expectedResult.add(String.format("Permananet Address :%s", permanentAdr));
 
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+
         getDriver().findElement(By.xpath("//div[@class='card-body']/h5[text()='Elements']")).click();
         getDriver().findElement(By.xpath("//div[@class='element-list collapse show']//li[@id='item-0']")).click();
         getDriver().findElement(By.id("userName")).sendKeys(name);
         getDriver().findElement(By.id("userEmail")).sendKeys(email);
         getDriver().findElement(By.id("currentAddress")).sendKeys(currentAdr);
         getDriver().findElement(By.id("permanentAddress")).sendKeys(permanentAdr);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("submit")));
         getDriver().findElement(By.id("submit")).click();
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("name")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='output']/div")));
 
         final List<String> actualResult = new ArrayList<>();
         actualResult.add(getDriver().findElement(By.id("name")).getText());
