932d8fa1171e1ea6c0f4b6025f41c906eab1baa3
NataliiaOliver <103964206+NataliiaOliver@users.noreply.github.com>
2022-11-10 22:35:55 -0600

    fix Ignore TestGroupRedTest (#421)

--- src/test/java/GroupCodeRedTest.java
+++ src/test/java/GroupCodeRedTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 import java.time.Duration;
