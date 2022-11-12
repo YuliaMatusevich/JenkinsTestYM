529fdbf7e8352f13a50066b04976d401aad1dedf
YuliaMatusevich <115679291+YuliaMatusevich@users.noreply.github.com>
2022-11-11 22:03:11 -0600

    LogoSizeTest added (#456)

--- src/test/java/GroupLoxyTest.java
+++ src/test/java/GroupLoxyTest.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupLoxyTest extends BaseTest {
+    public static final String URL_PARABANK = "https://parabank.parasoft.com/parabank/index.htm";
     @Test
     public void test_customerServiceResponseText_WhenContactFormSubmitted() {
-        final String url = "https://parabank.parasoft.com/parabank/index.htm";
         final String name = "Mary Johnson";
         final String email = "mj@test.com";
         final String phone = "1234567";
@@ -19,7 +19,7 @@ public class GroupLoxyTest extends BaseTest {
         expectedResult.add("Thank you " + name);
         expectedResult.add("A Customer Care Representative will be contacting you.");
 
-        getDriver().get(url);
+        getDriver().get(URL_PARABANK);
         getDriver().findElement(By.xpath("//div[@id = 'headerPanel']//li[@class = 'contact']")).click();
         getDriver().findElement(By.id("name")).sendKeys(name);
         getDriver().findElement(By.id("email")).sendKeys(email);
@@ -33,6 +33,14 @@ public class GroupLoxyTest extends BaseTest {
         actualResult.add(getDriver().findElement(By.xpath(".//div[@id = 'rightPanel']//p[2]")).getText());
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+    @Test
+    public void testTopPanelLogoSize_OnHomePage_YM(){
+        getDriver().get(URL_PARABANK);
+        Assert. assertEquals(getDriver().findElement(
+                By.className("logo")).getCssValue("width"), "136px");
+        Assert.assertEquals(getDriver().findElement(
+                By.className("logo")).getCssValue("height"), "31px");
     }
 
 }
