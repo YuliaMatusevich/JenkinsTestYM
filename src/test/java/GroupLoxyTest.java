1bbccd5b605237aa7030ddba62c6fdcd01ae0853
julykan <78547955+julykan@users.noreply.github.com>
2022-11-12 08:05:04 -0600

    new test added (#457)
    Co-authored-by: Julykan <julykan777@gmail.com>

--- src/test/java/GroupLoxyTest.java
+++ src/test/java/GroupLoxyTest.java
@@ -2,8 +2,14 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class GroupLoxyTest extends BaseTest {
     public static final String URL_PARABANK = "https://parabank.parasoft.com/parabank/index.htm";
@@ -42,5 +48,22 @@ public class GroupLoxyTest extends BaseTest {
         Assert.assertEquals(getDriver().findElement(
                 By.className("logo")).getCssValue("height"), "31px");
     }
+
+    @Test
+    public void testScheduleVisitFormJK() {
+        getDriver().get("https://www.fitzdentaldallas.com/");
+        getDriver().findElement(By.xpath("//div[@id='main']/div//ul/li/a[text()='Wisdom Tooth Removal']")).click();
+        getDriver().findElement(By.id("name1")).sendKeys("JuliaTestova");
+        getDriver().findElement(By.id("email1")).sendKeys("testgmail.com");
+        getDriver().findElement(By.id("phone1")).sendKeys("12345");
+        getDriver().findElement(By.xpath("//input[@value='Contact Us']")).click();
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//div[@id='wpcf7-f258-o1']/form/div[@class='wpcf7-response-output']")));
+
+        Assert.assertTrue(getDriver().findElement(
+                By.xpath("//div[@id='wpcf7-f258-o1']/form/div[@class='wpcf7-response-output']")).isDisplayed());
+    }
+
 
 }
