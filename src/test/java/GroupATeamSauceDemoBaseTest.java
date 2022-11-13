435a82c6a310e936d8924b6fefd30cd16b66d573
janarkqa <114713903+janarkqa@users.noreply.github.com>
2022-11-13 11:50:59 -0600

    GroupATeamSauceDemoTest | Some refactoring, some new tests (#470)
    * Add some tests

--- src/test/java/GroupATeamSauceDemoBaseTest.java
+++ src/test/java/GroupATeamSauceDemoBaseTest.java
@@ -1,21 +1,19 @@
-package group_a_team_test;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import runner.BaseTest;
 
-public abstract class SauceDemoBaseTest extends BaseTest {
+public abstract class GroupATeamSauceDemoBaseTest extends BaseTest {
     private Actions action;
 
-    protected Actions getAction() {
+    protected final Actions getAction() {
         if (action == null) {
             return new Actions(getDriver());
         }
         return action;
     }
 
-    protected void loginIn(String username, String password) {
-        getDriver().get(SauceDemoConsts.URL);
+    protected final void loginIn(String username, String password) {
+        getDriver().get(GroupATeamSauceDemoUtils.URL_SAUCE_DEMO);
         getAction().
                 moveToElement(getDriver().findElement(By.id("user-name"))).click().sendKeys(username).
                 moveToElement(getDriver().findElement(By.id("password"))).click().sendKeys(password).
