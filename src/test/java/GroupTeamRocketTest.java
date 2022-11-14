cd1136b75395ec200a740224b1da1fe89666cfd2
NataliiaOliver <103964206+NataliiaOliver@users.noreply.github.com>
2022-11-13 22:03:24 -0600

    RF testFindTitleGuide_NataliiaOliver(), delite sleeps, added wait (#495)

--- src/test/java/GroupTeamRocketTest.java
+++ src/test/java/GroupTeamRocketTest.java
@@ -60,15 +60,16 @@ public class GroupTeamRocketTest extends BaseTest {
     }
 
     @Test
-    public void testFindTitleGuide_NataliiaOliver() throws InterruptedException {
+    public void testFindTitleGuide_NataliiaOliver() {
         getDriver().get("https://openweathermap.org/");
 
-        Thread.sleep(10000);
+        new WebDriverWait(getDriver(),Duration.ofSeconds(20)).until(ExpectedConditions.invisibilityOfElementLocated(
+                By.className("owm-loader-container")));
 
-        getDriver().findElement(By.xpath("//div[@id='desktop-menu']/ul/li/a[@href='/guide']")).click();
-        Thread.sleep(1000);
+        WebElement guide = getDriver().findElement(By.xpath("//div[@id='desktop-menu']/ul/li/a[@href='/guide']"));
+        new WebDriverWait(getDriver(), Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(guide));
+        new Actions(getDriver()).click(guide).perform();
 
-        Assert.assertEquals(getDriver().getCurrentUrl(), "https://openweathermap.org/guide");
         Assert.assertEquals(
                 getDriver().findElement(By.xpath("//div[@class='col-sm-7']/h1[text()='Guide']")).getText(),
                 "Guide");
