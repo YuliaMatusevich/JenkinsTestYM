e67e227a703d3cf531a42d3816d83cf9c3522864
AlekseiChapaev <93160447+AlekseiChapaev@users.noreply.github.com>
2022-11-16 00:28:10 +0100

    AT 00.01.001 New item > Freestyle Project> Create Freestyle Project (#520)
     AT 00.01.001 New item > Freestyle Project> Create Freestyle Project add testCreateNewFreestyleProject()

---
+++ src/test/java/FreestyleProjectTest.java
@@ -0,0 +1,38 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.time.Duration;
+
+public class FreestyleProjectTest extends BaseTest {
+
+    private WebDriverWait wait;
+    private final String freestyleName = RandomStringUtils.randomAlphanumeric(10);
+
+    private WebDriverWait getWait() {
+        if (wait == null) {
+            wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+        }
+
+        return wait;
+    }
+
+    @Test
+    public void testCreateNewFreestyleProject() {
+        getWait().until(ExpectedConditions.elementToBeClickable(By.linkText("New Item"))).click();
+
+        final WebElement itemNameField = getDriver().findElement(By.id("name"));
+        itemNameField.click();
+        itemNameField.sendKeys(freestyleName);
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        getDriver().findElement(By.xpath("//span[@name = 'Submit']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + freestyleName);
+    }
+}
