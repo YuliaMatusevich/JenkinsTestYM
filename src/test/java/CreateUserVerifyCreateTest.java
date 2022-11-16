64183223f2c41f5815682ef1018aca4d0cabb922
bahasadyr <105130043+bahasadyr@users.noreply.github.com>
2022-11-16 17:10:46 -0600

    AT 07.03.01 | | People > People > Delete user (#523)
    * added delete after user test
    
    * refactored xpath

--- src/test/java/CreateUserVerifyCreateTest.java
+++ src/test/java/CreateUserVerifyCreateTest.java
@@ -1,15 +1,20 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
 public class CreateUserVerifyCreateTest extends BaseTest {
+
+    private String randUserName = getRandomDigitAndLetterString();
 
 
     public static String getRandomDigitAndLetterString() {
@@ -27,7 +32,6 @@ public class CreateUserVerifyCreateTest extends BaseTest {
     @Test
     public void testCreateUser() throws InterruptedException {
 
-        String randUserName = getRandomDigitAndLetterString();
         String randFullName = getRandomDigitAndLetterString();
         String randEmail = getRandomDigitAndLetterString();
         String randPass = getRandomDigitAndLetterString();
@@ -55,6 +59,29 @@ public class CreateUserVerifyCreateTest extends BaseTest {
 
         Assert.assertTrue(lstStringWithNamesOnPeoplePage.contains(randUserName));
 
+    }
+
+    @Test(dependsOnMethods = "testCreateUser")
+    public void testToDeleteUser() {
+
+        getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(getDriver().findElement(By.xpath("//a[@href='securityRealm/']"))).click().perform();
+        WebElement deleteButton = getDriver().findElement(By.xpath("//a[@href='user/"+randUserName.toLowerCase()+"/delete']"));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        List<WebElement> listWithUsersIncludingAdmin = getDriver().findElements(By.xpath("//table[@id='people']/tbody/tr/td[2]/a"));
+        List<String> listStringov = listWithUsersIncludingAdmin.stream().map(WebElement::getText).collect(Collectors.toList());
+        if (listStringov.contains(randUserName)) {
+            deleteButton.click();
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//form[@action='doDelete']")));
+            getDriver().findElement(By.cssSelector("#yui-gen1-button")).click();
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h1[text()='Users']")));
+        }
+        List<WebElement> listWithUsersIncludingAdmin2 = getDriver().findElements(By.xpath("//table[@id='people']/tbody/tr/td[2]/a"));
+        List<String> listStringovFinal = listWithUsersIncludingAdmin2.stream().map(WebElement::getText).collect(Collectors.toList());
+
+
+        Assert.assertFalse(listStringovFinal.contains(randUserName));
     }
 
 }
