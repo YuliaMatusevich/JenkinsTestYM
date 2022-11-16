20bf572f3df05e89085dcc9c5539b8516d7cbf4c
eapiskunova <eapiskunova@gmail.com>
2022-11-16 11:36:38 -0800

    AT 04.04 | Folder > Delete Folder modifier random String

--- src/test/java/FolderTest.java
+++ src/test/java/FolderTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import runner.BaseTest;
 
 import java.util.List;
-import java.util.Random;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class FolderTest extends BaseTest {
@@ -92,17 +92,7 @@ public class FolderTest extends BaseTest {
 
     @Test
     public void deleteFolder() {
-        int leftLimit = 97;
-        int rightLimit = 122;
-        int targetStringLength = 10;
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        String generatedString = buffer.toString();
+        String generatedString = UUID.randomUUID().toString();
         getDriver().findElement(By.linkText("New Item")).click();
         getInputName().sendKeys(generatedString);
         getFolder().click();
