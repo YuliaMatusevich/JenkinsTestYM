bc4e839e8747381d789aa437d95931752a3e1690
Valentin Kuprin (xbrookx) <72659405+ValentinKuprin@users.noreply.github.com>
2022-11-18 11:03:56 +0300

    AT 00.07.01 | New Item > New Item Page | Contains Items without created project (#537)

---
+++ src/test/java/NewItemTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NewItemTest extends BaseTest {
+
+    @Test
+    public void testNewItemsPageContainsItemsWithoutCreatedProject() {
+        final List<String> expectedResult = List.of("Freestyle project", "Pipeline", "Multi-configuration project",
+                "Folder", "Multibranch Pipeline", "Organization Folder");
+
+        getDriver().findElement(By.linkText("New Item")).click();
+
+        List<WebElement> newItemsElements = getDriver().findElements(By.xpath("//label/span[@class='label']"));
+        List<String> newItemsName = newItemsElements.stream().map(WebElement::getText).collect(Collectors.toList());
+
+        Assert.assertEquals(newItemsName, expectedResult);
+    }
+}
