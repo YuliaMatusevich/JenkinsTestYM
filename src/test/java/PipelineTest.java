f7d419af0f72f488be451b4bb7f937f5ae67cfee
DmitriiB <117135719+dmcalifornia@users.noreply.github.com>
2022-11-19 19:16:57 -0800

    Pipeline test is added (#618)

---
+++ src/test/java/PipelineTest.java
@@ -0,0 +1,50 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class PipelineTest extends BaseTest {
+
+    private static final String PIPELINE_NAME = RandomStringUtils.randomAlphanumeric(10);
+    private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
+    private static final By ITEM_NAME = By.id("name");
+    private static final By PIPELINE = By.xpath("//*[text() = 'Pipeline']");
+    private static final By BUTTON_OK = By.id("ok-button");
+    private static final By BUTTON_SAVE = By.id("yui-gen6-button");
+    private static final By JENKINS_ICON = By.id("jenkins-name-icon");
+    private static final By BUTTON_DISABLE_PROJECT = By.id("yui-gen1-button");
+    private static final By PROJECT =
+            By.xpath(String.format("//td/a/span[contains(text(),'%s')]", PIPELINE_NAME));
+
+    private void createPipelineProject() {
+        getDriver().findElement(NEW_ITEM).click();
+        getDriver().findElement(PIPELINE).click();
+        getDriver().findElement(ITEM_NAME).sendKeys(PIPELINE_NAME);
+        getDriver().findElement(BUTTON_OK).click();
+        getDriver().findElement(BUTTON_SAVE).click();
+
+        getDriver().findElement(JENKINS_ICON).click();
+    }
+
+    private void deletePipelineProject() {
+        getDriver().findElement(JENKINS_ICON).click();
+        getDriver().findElement(PROJECT).click();
+        getDriver().findElement(By.cssSelector("svg.icon-edit-delete")).click();
+        getDriver().switchTo().alert().accept();
+    }
+
+    @Test
+    public void testDisablePipelineProjectMessage() {
+        createPipelineProject();
+
+        getDriver().findElement(JENKINS_ICON).click();
+        getDriver().findElement(PROJECT).click();
+        getDriver().findElement(BUTTON_DISABLE_PROJECT).click();
+
+        Assert.assertTrue(getDriver().findElement(By.id("enable-project")).getText()
+                .contains("This project is currently disabled"));
+
+        deletePipelineProject();
+    }
+}
