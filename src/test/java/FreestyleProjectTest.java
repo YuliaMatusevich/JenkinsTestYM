aee2f53358430b7427c3ccee241f0a498de04c94
MaximWho <112827127+MaximWho@users.noreply.github.com>
2022-11-20 16:11:55 +0200

    AT 01.07.02 | Freestyle Project> Creating Freestyle Project With a Description (#623)
    * Create Freestyle Project With Description

--- src/test/java/FreestyleProjectTest.java
+++ src/test/java/FreestyleProjectTest.java
@@ -97,4 +97,24 @@ public class FreestyleProjectTest extends BaseTest {
 
         Assert.assertFalse(getListExistingFreestyleProjectsNames(LIST_FREESTYLE_JOBS).contains(NEW_FREESTYLE_NAME));
     }
+
+    @Test
+    public void testCreateFreestyleProjectWithDescription() {
+        final String name = "JustName";
+
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(name);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("Some Description Text");
+
+        getDriver().findElement(By.id("yui-gen23-button")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='description']/div[1]")).getText(),
+                "Some Description Text");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main-panel']/h1")).getText(),
+                String.format("Project %s", name));
+    }
 }
+
+
