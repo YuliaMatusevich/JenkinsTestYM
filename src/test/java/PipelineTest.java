8ac7a268a185d52a9fcf087e072708af9a90281b
Arailym <33895096+ArailymAsar@users.noreply.github.com>
2022-11-20 23:17:26 +0600

    test Created Pipeline Displayed On My Views (#630)
    * test Created Pipeline Displayed On My Views
    
    * Ignore testDisablePipelineProjectMessage

--- src/test/java/PipelineTest.java
+++ src/test/java/PipelineTest.java
@@ -1,6 +1,7 @@
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -16,6 +17,8 @@ public class PipelineTest extends BaseTest {
     private static final By BUTTON_DISABLE_PROJECT = By.id("yui-gen1-button");
     private static final By PROJECT =
             By.xpath(String.format("//td/a/span[contains(text(),'%s')]", PIPELINE_NAME));
+
+    private static final By DASHBOARD = By.xpath("//a[text()='Dashboard']");
 
     private void createPipelineProject() {
         getDriver().findElement(NEW_ITEM).click();
@@ -34,6 +37,7 @@ public class PipelineTest extends BaseTest {
         getDriver().switchTo().alert().accept();
     }
 
+    @Ignore
     @Test
     public void testDisablePipelineProjectMessage() {
         createPipelineProject();
@@ -46,5 +50,14 @@ public class PipelineTest extends BaseTest {
                 .contains("This project is currently disabled"));
 
         deletePipelineProject();
+    }
+    @Test
+    public void testCreatedPipelineDisplayedOnMyViews() {
+
+        createPipelineProject();
+        getDriver().findElement(DASHBOARD).click();
+        getDriver().findElement(By.xpath("//a[@href='/me/my-views']")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='job/" + PIPELINE_NAME + "/']")).isDisplayed());
     }
 }
