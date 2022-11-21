e60172a62589b2a33fb20ff947daf5724c0f3356
Babintseva Svetlana <83321269+BabintsevaS@users.noreply.github.com>
2022-11-20 17:48:23 -0800

    RF | NewViewTest> testCreateNewView (#643)
    * refactor test new view
    
    * changed assert
    
    * fixed xpath in deletePipelineProject

--- src/test/java/NewViewTest.java
+++ src/test/java/NewViewTest.java
@@ -6,7 +6,7 @@ import runner.BaseTest;
 
 public class NewViewTest extends BaseTest {
 
-    private static final String PIPELINE_NAME = "testpipelineproject";
+    private static final String PIPELINE_NAME = RandomStringUtils.randomAlphanumeric(5);
     private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
     private static final By ITEM_NAME = By.id("name");
     private static final By PIPELINE = By.xpath("//*[text() = 'Pipeline']");
@@ -16,32 +16,36 @@ public class NewViewTest extends BaseTest {
     private static final By MY_VIEWS = By.xpath("//a[@href='/me/my-views']");
     private static final By ADD_TAB = By.className("addTab");
     private static final By VIEW_NAME_FIELD = By.id("name");
-    private static final String VIEW_NAME = "test_view";
-    private static final By RADIO_BUTTON_MY_VIEW = By.xpath("//*[@id='createItemForm']/div[1]/div[2]/fieldset/div[3]/label");
+    private static final String VIEW_NAME = RandomStringUtils.randomAlphanumeric(5);
+    private static final By RADIO_BUTTON_MY_VIEW =
+            By.xpath("//*[@id='createItemForm']/div[1]/div[2]/fieldset/div[3]/label");
     private static final By BUTTON_CREATE = By.id("ok");
-    private static final By PROJECT =
-            By.xpath(String.format("//td/a/span[contains(text(),'%s')]", PIPELINE_NAME));
-
+    private static final By BUTTON_DELETE = By.cssSelector("svg.icon-edit-delete");
+    private static final By VIEW =
+            By.xpath(String.format("//div/a[contains(text(),'%s')]", VIEW_NAME));
 
     private void createPipelineProject() {
+        getDriver().findElement(JENKINS_ICON).click();
         getDriver().findElement(NEW_ITEM).click();
         getDriver().findElement(PIPELINE).click();
         getDriver().findElement(ITEM_NAME).sendKeys(PIPELINE_NAME);
         getDriver().findElement(BUTTON_OK).click();
         getDriver().findElement(BUTTON_SAVE).click();
-
         getDriver().findElement(JENKINS_ICON).click();
     }
 
     private void deletePipelineProject() {
-        getDriver().findElement(PROJECT).click();
-        getDriver().findElement(By.cssSelector("svg.icon-edit-delete")).click();
+        getDriver().findElement(JENKINS_ICON).click();
+        getDriver().findElement(By.xpath(String.format("//a[@href = contains(., '%s')]/button", PIPELINE_NAME))).click();
+        getDriver().findElement(BUTTON_DELETE).click();
         getDriver().switchTo().alert().accept();
     }
 
     private void deleteNewView() {
-        getDriver().findElement(By.xpath("//*[@id='projectstatus-tabBar']/div/div[1]/div[2]/a")).click();
-        getDriver().findElement(By.cssSelector("svg.icon-edit-delete")).click();
+        getDriver().findElement(JENKINS_ICON).click();
+        getDriver().findElement(MY_VIEWS).click();
+        getDriver().findElement(VIEW).click();
+        getDriver().findElement(BUTTON_DELETE).click();
         getDriver().findElement(By.id("yui-gen1-button")).click();
     }
 
@@ -55,8 +59,7 @@ public class NewViewTest extends BaseTest {
         getDriver().findElement(RADIO_BUTTON_MY_VIEW).click();
         getDriver().findElement(BUTTON_CREATE).click();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='projectstatus-tabBar']/div/div[1]/div[2]/a")).getText(),
-                "test_view");
+        Assert.assertTrue(getDriver().findElement(By.xpath(String.format("//div/a[contains(text(),'%s')]", VIEW_NAME))).isDisplayed());
 
         deleteNewView();
         deletePipelineProject();
