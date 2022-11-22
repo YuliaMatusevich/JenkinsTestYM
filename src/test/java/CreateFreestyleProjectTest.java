5746d73e0a9832689e4a2f0ba498109ce6f84d36
Kristi-W <wkristi.study@gmail.com>
2022-11-21 21:52:12 -0500

    AT 00.01.02 | New item > Freestyle Project> Create Freestyle Project Using Invalid Name (#670)
    * Fixed the testUrlTitleHeadingOfPageAbout test and cleaned code
    
    * Committing these files because during pull I got error "Untracked Files Prevent Pull" - options: move them or commit
    
    * added test testCreateFreestyleProjectWithInvalidName
    
    * added line before Assert
    
    Co-authored-by: kristi-w <wkristi.study@gamil.com>

--- src/test/java/CreateFreestyleProjectTest.java
+++ src/test/java/CreateFreestyleProjectTest.java
@@ -1,11 +1,16 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+import java.time.Duration;
 
 public class CreateFreestyleProjectTest extends BaseTest {
 
     private static final String VALID_FREESTYLE_PROJECT_NAME = "First project";
+    private static final Character INVALID_CHAR = '!';
+    private static final String INVALID_FREESTYLE_PROJECT_NAME = INVALID_CHAR + "First project";
     private static final String DESCRIPTION = "Description Text";
     private static final By BUTTON_OK = By.xpath("//button[@type = 'submit']");
     private static final By BUTTON_SAVE = By.xpath("//button[@type = 'submit']");
@@ -23,6 +28,17 @@ public class CreateFreestyleProjectTest extends BaseTest {
                 "Project " + VALID_FREESTYLE_PROJECT_NAME);
         Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='description']/div[1]")).getText(),
                 DESCRIPTION);
+    }
+    @Test
+    public void testCreateFreestyleProjectWithInvalidName() {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(15));
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(INVALID_FREESTYLE_PROJECT_NAME);
+        getDriver().findElement(By.xpath("//form/div[2]/div[1]/ul/li[1]")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("itemname-invalid")));
+
+        Assert.assertEquals(getDriver().findElement(By.id("itemname-invalid")).getText(),
+                "» ‘" + INVALID_CHAR + "’ is an unsafe character");
     }
 }
 
