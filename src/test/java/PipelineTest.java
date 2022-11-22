3bf0b640a179e1b278356a4f05d9c8769aada7b0
DmitriiB <117135719+dmcalifornia@users.noreply.github.com>
2022-11-21 17:03:47 -0800

    RF | PipelineTest> testDisablePipelineProjectMessage (#664)
    * Refactor the test class PipelineTest
    
    * Ignore import is removed.

--- src/test/java/PipelineTest.java
+++ src/test/java/PipelineTest.java
@@ -1,63 +1,57 @@
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class PipelineTest extends BaseTest {
 
-    private static final String PIPELINE_NAME = RandomStringUtils.randomAlphanumeric(10);
     private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
     private static final By ITEM_NAME = By.id("name");
     private static final By PIPELINE = By.xpath("//*[text() = 'Pipeline']");
     private static final By BUTTON_OK = By.id("ok-button");
     private static final By BUTTON_SAVE = By.id("yui-gen6-button");
-    private static final By JENKINS_ICON = By.id("jenkins-name-icon");
     private static final By BUTTON_DISABLE_PROJECT = By.id("yui-gen1-button");
-    private static final By PROJECT =
-            By.xpath(String.format("//td/a/span[contains(text(),'%s')]", PIPELINE_NAME));
-
     private static final By DASHBOARD = By.xpath("//a[text()='Dashboard']");
 
-    private void createPipelineProject() {
+    private String generatePipelineProjectName() {
+        return RandomStringUtils.randomAlphanumeric(10);
+    }
+
+    private void createPipelineProject(String projectName) {
+
         getDriver().findElement(NEW_ITEM).click();
         getDriver().findElement(PIPELINE).click();
-        getDriver().findElement(ITEM_NAME).sendKeys(PIPELINE_NAME);
+        getDriver().findElement(ITEM_NAME).sendKeys(projectName);
         getDriver().findElement(BUTTON_OK).click();
         getDriver().findElement(BUTTON_SAVE).click();
-
-        getDriver().findElement(JENKINS_ICON).click();
+        getDriver().findElement(DASHBOARD).click();
     }
 
-    private void deletePipelineProject() {
-        getDriver().findElement(JENKINS_ICON).click();
-        getDriver().findElement(PROJECT).click();
-        getDriver().findElement(By.cssSelector("svg.icon-edit-delete")).click();
-        getDriver().switchTo().alert().accept();
-    }
-
-    @Ignore
     @Test
     public void testDisablePipelineProjectMessage() {
-        createPipelineProject();
 
-        getDriver().findElement(JENKINS_ICON).click();
-        getDriver().findElement(PROJECT).click();
+        String pipelinePojectName = generatePipelineProjectName();
+        createPipelineProject(pipelinePojectName);
+
+        getDriver().findElement(DASHBOARD).click();
+        getDriver().findElement(By.xpath(String.format("//td/a/span[contains(text(),'%s')]", pipelinePojectName))).click();
         getDriver().findElement(BUTTON_DISABLE_PROJECT).click();
 
         Assert.assertTrue(getDriver().findElement(By.id("enable-project")).getText()
                 .contains("This project is currently disabled"));
-
-        deletePipelineProject();
     }
+
     @Test
     public void testCreatedPipelineDisplayedOnMyViews() {
 
-        createPipelineProject();
+        String pipelinePojectName = generatePipelineProjectName();
+        createPipelineProject(pipelinePojectName);
+
         getDriver().findElement(DASHBOARD).click();
         getDriver().findElement(By.xpath("//a[@href='/me/my-views']")).click();
 
-        Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='job/" + PIPELINE_NAME + "/']")).isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='job/" + pipelinePojectName + "/']"))
+                .isDisplayed());
     }
 }
