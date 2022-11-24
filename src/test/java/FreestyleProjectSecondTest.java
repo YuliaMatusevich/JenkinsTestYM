11026ac34cdf17da08ebe3fbe2580eea04dd4634
MykhailoMo <114332867+MykhailoMo@users.noreply.github.com>
2022-11-24 08:35:13 +0100

    AT 01.07.004 | Freestyle Project> Add Description to Freestyle Project (#713)
    * AT01.07.004
    
    * AT01.07.004
    
    * AT01.07.004

--- src/test/java/FreestyleProjectSecondTest.java
+++ src/test/java/FreestyleProjectSecondTest.java
@@ -8,29 +8,24 @@ import runner.BaseTest;
 public class FreestyleProjectSecondTest extends BaseTest {
     private static final String FREESTYLE_NAME = RandomStringUtils.randomAlphanumeric(10);
     private static final String NEW_FREESTYLE_NAME = RandomStringUtils.randomAlphanumeric(10);
-    public void createFreestyleProject() {
+    private static final String DESCRIPTION_TEXT = RandomStringUtils.randomAlphanumeric(20);
+
+    @Test
+    public void testCreateFreestyleProject(){
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//input[@class='jenkins-input']")).sendKeys(FREESTYLE_NAME);
         getDriver().findElement(By.xpath("//img[@class='icon-freestyle-project icon-xlg']")).click();
         getDriver().findElement(By.id("ok-button")).click();
-    }
-
-    @Test
-    public void testCreateFreestyleProject(){
-
-        createFreestyleProject();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//li/a[@href='/job/" + FREESTYLE_NAME + "/']"))
                 .getText(), FREESTYLE_NAME);
     }
 
-    @Test
+    @Test(dependsOnMethods = "testCreateFreestyleProject")
     public void testCreateAndRenameFreestyleProject(){
 
-        createFreestyleProject();
-
-        getDriver().findElement(By.xpath("//li/a[@href='/job/" + FREESTYLE_NAME + "/']")).click();
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + FREESTYLE_NAME + "/']")).click();
         getDriver().findElement(By.xpath("//span/a[@href='/job/" + FREESTYLE_NAME + "/confirm-rename']")).click();
         WebElement newName = getDriver().findElement(By.xpath("//div/input[@checkdependson='newName']"));
         newName.clear();
@@ -39,5 +34,17 @@ public class FreestyleProjectSecondTest extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//li/a[@href='/job/" + NEW_FREESTYLE_NAME + "/']"))
                 .getText(), NEW_FREESTYLE_NAME);
+    }
+
+    @Test(dependsOnMethods = "testCreateAndRenameFreestyleProject")
+    public void testCreateWithDescriptionFreestyleProject(){
+
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + NEW_FREESTYLE_NAME + "/']")).click();
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.xpath("//div/textarea[@name='description']")).sendKeys(DESCRIPTION_TEXT);
+        getDriver().findElement(By.xpath("//span/button[@type='submit']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='description']/div[1]")).getText(),
+                DESCRIPTION_TEXT);
     }
 }
