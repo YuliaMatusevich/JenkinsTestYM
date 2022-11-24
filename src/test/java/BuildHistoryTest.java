f66140ae9aa9ad1fec12cc675ffbb4d3b67905ec
Happy Strawberry <85979179+Iriskus@users.noreply.github.com>
2022-11-24 11:49:22 -0600

    AT 12.02.05 | Header > Redirect from the "Build History" page to the homepage (#719)
    test VerifyRedirectToMainPage refactored and added

--- src/test/java/BuildHistoryTest.java
+++ src/test/java/BuildHistoryTest.java
@@ -1,6 +1,19 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class BuildHistoryTest extends BaseTest {
 
 
+    @Test
+    public void testVerifyRedirectToMainPage() {
+        getDriver().findElement(
+                By.linkText("Build History")).click();
+
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.cssSelector("#description-link")).getText(),
+                "Add description");
+    }
 }
