116d8b54f284e73e65051d031c36a80099030afb
MaksPt <100244529+MaksPt@users.noreply.github.com>
2022-11-27 12:29:06 +0300

    AT 09.02.02 | Manage Jenkins>Security>Manage Users>Rename Full Name (#766)
    * AT 09.02.02 | Manage Jenkins > Security > Manage Users > Rename Full Name
    Class created for the test:
    ManageJenkinsTest
    Test created:
    testChangeFullUserName()

---
+++ src/test/java/ManageJenkinsTest.java
@@ -0,0 +1,31 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class ManageJenkinsTest extends BaseTest {
+
+    @Test
+    public void testRenameFullUserName() {
+        final String newFullName = RandomStringUtils.randomAlphanumeric(8);
+
+        getDriver().findElement(By.linkText("Manage Jenkins")).click();
+        getDriver().findElement(By.xpath("//a[@href='securityRealm/']")).click();
+        String UserIDName = getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText();
+        getDriver().findElement(By.className("jenkins-table__button")).click();
+        getDriver().findElement(By.name("_.fullName")).clear();
+        getDriver().findElement(By.name("_.fullName")).sendKeys(newFullName);
+        getDriver().findElement(By.id("yui-gen3-button")).click();
+        getDriver().navigate().refresh();
+
+        String actualFullNameOnBreadCrumbs = getDriver().findElement(
+                By.xpath("//a[@href='/manage/securityRealm/user/" + UserIDName + "/']")).getText();
+        String actualFullNameOnPageHeader = getDriver().findElement(
+                By.xpath("//a[@href='/user/" + UserIDName + "']")).getText();
+
+        Assert.assertEquals(actualFullNameOnBreadCrumbs, newFullName);
+        Assert.assertEquals(actualFullNameOnPageHeader, newFullName);
+    }
+}
