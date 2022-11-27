82470fb139bffc335ad4dab938c6fb57f912a927
AlekseiChapaev <93160447+AlekseiChapaev@users.noreply.github.com>
2022-11-27 21:04:26 +0100

    Tc 01.04.07 freestyle project configure git source code (#769)
     add testConfigureSourceCodeGIT(), add getBuildStatus() method,

--- src/test/java/FreestyleProjectTest.java
+++ src/test/java/FreestyleProjectTest.java
@@ -1,7 +1,6 @@
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -40,9 +39,9 @@ public class FreestyleProjectTest extends BaseTest {
     private static final By BUILD_NOW_LOCATOR = By.linkText("Build Now");
     private static final By BUILDS_LOCATOR =
             By.xpath("//table[@class='pane jenkins-pane stripped']//tr[@page-entry-id]");
-    private static final By DISABLE_PROJECT_BUTTON = By.id("yui-gen1-button");
-    private static final By ENABLE_PROJECT_BUTTON = By.xpath("//button[@type = 'submit']");
+    private static final By ENABLE_DISABLE_PROJECT_BUTTON = By.id("yui-gen1-button");
     private static final By GO_TO_DASHBOARD_BUTTON = By.linkText("Dashboard");
+    private static final By CONFIGURE_BUTTON = By.linkText("Configure");
 
     private WebDriverWait wait;
 
@@ -57,8 +56,12 @@ public class FreestyleProjectTest extends BaseTest {
         return getDriver().findElements(by).stream().map(WebElement::getText).collect(Collectors.toList());
     }
 
-    private List<WebElement> findJobParam(String nameJob) {
-        return getDriver().findElements(By.xpath("//tr[@id = 'job_" + nameJob + "']"));
+    private List<WebElement> getJobSpecifications(String nameJob) {
+        return getDriver().findElements(By.xpath("//tr[@id = 'job_" + nameJob + "']/td"));
+    }
+
+    private String getBuildStatus(){
+        return getDriver().findElement(By.xpath("//span/span/*[name()='svg' and @class= 'svg-icon ']")).getAttribute("tooltip");
     }
 
     @Test
@@ -79,7 +82,7 @@ public class FreestyleProjectTest extends BaseTest {
     public void testDisableProject() {
 
         getDriver().findElement(By.xpath("//a[@href='job/" + FREESTYLE_NAME + "/']")).click();
-        getDriver().findElement(DISABLE_PROJECT_BUTTON).click();
+        getDriver().findElement(ENABLE_DISABLE_PROJECT_BUTTON).click();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + FREESTYLE_NAME);
         Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class = 'warning']")).getText().trim().substring(0, 34),
@@ -87,17 +90,17 @@ public class FreestyleProjectTest extends BaseTest {
 
         getDriver().findElement(GO_TO_DASHBOARD_BUTTON).click();
 
-        Assert.assertTrue(findJobParam(FREESTYLE_NAME).get(0).getAttribute("class").contains("job-status-disabled"));
+        Assert.assertEquals(getBuildStatus(), "Disabled");
     }
 
     @Test(dependsOnMethods = "testDisableProject")
     public void testEnableProject() {
 
         getDriver().findElement(By.xpath("//a[@href='job/" + FREESTYLE_NAME + "/']")).click();
-        getDriver().findElement(ENABLE_PROJECT_BUTTON).click();
+        getDriver().findElement(ENABLE_DISABLE_PROJECT_BUTTON).click();
         getDriver().findElement(GO_TO_DASHBOARD_BUTTON).click();
 
-        Assert.assertTrue(findJobParam(FREESTYLE_NAME).get(0).getAttribute("class").contains(" job-status-nobuilt"));
+        Assert.assertEquals(getBuildStatus(), "Not built");
     }
 
     @Test(dependsOnMethods = "testEnableProject")
@@ -147,7 +150,7 @@ public class FreestyleProjectTest extends BaseTest {
         Assert.assertEquals(actualChangesText, "No builds.");
     }
 
-    @Test(dependsOnMethods = "testNoBuildFreestyleProjectChanges")
+    @Test(dependsOnMethods = "testConfigureSourceCodeGIT")
     public void testRenameFreestyleProject() {
 
         getDriver().findElement(By.cssSelector("tr#job_" + FREESTYLE_NAME + " .jenkins-menu-dropdown-chevron")).click();
@@ -169,10 +172,9 @@ public class FreestyleProjectTest extends BaseTest {
                 , String.format("Project %s", NEW_FREESTYLE_NAME));
     }
 
-
     @Test(dependsOnMethods = "testViewFreestyleProjectPage")
     public void testViewChangesNoBuildsSignAppears() {
-        String expectedText = "Changes\nNo builds.";
+        String expectedText = "Changes\nNo changes in any of the builds.";
 
         getDriver().findElement(By.xpath("//span[contains(text(),'" + NEW_FREESTYLE_NAME + "')]")).click();
         getDriver().findElement(LINK_CHANGES).click();
@@ -185,7 +187,7 @@ public class FreestyleProjectTest extends BaseTest {
     @Test(dependsOnMethods = "testViewChangesNoBuildsSignAppears")
     public void testFreestyleProjectConfigureByDropdown() {
         getDriver().findElement(By.cssSelector("#job_" + NEW_FREESTYLE_NAME + " .jenkins-menu-dropdown-chevron")).click();
-        WebElement element = getDriver().findElement(By.xpath("//a[@href='/job/" + NEW_FREESTYLE_NAME + "/configure']"));
+        WebElement element = getDriver().findElement(CONFIGURE_BUTTON);
         JavascriptExecutor executor = (JavascriptExecutor) getDriver();
         executor.executeScript("arguments[0].click();", element);
 
@@ -195,7 +197,7 @@ public class FreestyleProjectTest extends BaseTest {
     @Test(dependsOnMethods = "testFreestyleProjectConfigureByDropdown")
     public void testFreestyleProjectConfigureMenu() {
         getDriver().findElement(By.xpath("//a[@href='job/" + NEW_FREESTYLE_NAME + "/']")).click();
-        getDriver().findElement(By.xpath("//a[@href='/job/" + NEW_FREESTYLE_NAME + "/configure']")).click();
+        getDriver().findElement(CONFIGURE_BUTTON).click();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//button[@data-section-id='general']"))
                 .getText(), "General");
@@ -402,5 +404,30 @@ public class FreestyleProjectTest extends BaseTest {
         Assert.assertEquals(getDriver().findElement(DESCRIPTION_TEXT).getText(),
                 DESCRIPTION_INPUT);
 
+    }
+
+    @Test(dependsOnMethods = "testNoBuildFreestyleProjectChanges")
+    public void testConfigureSourceCodeGIT() {
+        final String repositoryURL = "https://github.com/AlekseiChapaev/TestingJenkinsRepo.git";
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+
+        getDriver().findElement(By.xpath("//a[@href='job/" + FREESTYLE_NAME + "/']")).click();
+        getDriver().findElement(CONFIGURE_BUTTON).click();
+        getDriver().findElement(By.xpath("//button[@data-section-id= 'source-code-management']")).click();
+        getDriver().findElement(By.xpath("//label[text() = 'Git']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@name= 'userRemoteConfigs']/div[@style]/div[1][@class = 'jenkins-form-item tr  has-help']/div[2]"))).click();
+        getDriver().findElement(By.xpath("//div[@name= 'userRemoteConfigs']/div[@style]/div[1][@class = 'jenkins-form-item tr  has-help']/div[2]/input")).sendKeys(repositoryURL);
+        getDriver().findElement(BUTTON_SAVE).click();
+
+        getDriver().findElement(GO_TO_DASHBOARD_BUTTON).click();
+        getJobSpecifications(FREESTYLE_NAME).get(6).click();
+
+        while(getJobSpecifications(FREESTYLE_NAME).get(5).getText().equals("N/A")){
+            getDriver().navigate().refresh();
+        }
+
+        Assert.assertEquals(getBuildStatus(), "Success");
+        Assert.assertTrue(getJobSpecifications(FREESTYLE_NAME).get(3).getText().contains("#1"));
     }
 }
