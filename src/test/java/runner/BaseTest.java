e2ab0558f4fb7b0c82d29adaf66f8a62e90e952f
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2022-11-28 12:28:59 -0500

    refactoring

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -41,7 +41,7 @@ public abstract class BaseTest {
                 getWeb();
             }
         } catch (Exception e) {
-            stopDriver();
+            closeDriver();
             throw new RuntimeException(e);
         } finally {
             methodsOrder.markAsInvoked(method);
@@ -74,8 +74,14 @@ public abstract class BaseTest {
             ProjectUtils.logout(driver);
         } catch (Exception ignore) {}
 
-        driver.quit();
-        BaseUtils.log("Browser closed");
+        closeDriver();
+    }
+
+    protected void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            BaseUtils.log("Browser closed");
+        }
     }
 
     @AfterMethod
