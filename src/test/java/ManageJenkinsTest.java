994ceffca98546acd3e016725cf180a5d05787b3
Ilya Korolkov <il9.korolkov@gmail.com>
2022-11-28 17:43:13 +0500

    AT 09.04.01 | Manage Jenkins > Troubleshooting > Manage Old Data (No data) (#775)

--- src/test/java/ManageJenkinsTest.java
+++ src/test/java/ManageJenkinsTest.java
@@ -1,11 +1,20 @@
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class ManageJenkinsTest extends BaseTest {
+
+    private static final By MANAGE_JENKINS = By.linkText("Manage Jenkins");
+
+    public static void jsClick(WebDriver driver, WebElement element) {
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();", element);
+    }
 
     @Test
     public void testRenameFullUserName() {
@@ -27,5 +36,21 @@ public class ManageJenkinsTest extends BaseTest {
 
         Assert.assertEquals(actualFullNameOnBreadCrumbs, newFullName);
         Assert.assertEquals(actualFullNameOnPageHeader, newFullName);
+    }
+
+    @Test
+    public void testManageOldData() {
+
+        final String expectedText = "No old data was found.";
+
+        getDriver().findElement(MANAGE_JENKINS).click();
+
+        jsClick(getDriver(), getDriver().findElement(By.xpath("//a[@href='administrativeMonitor/OldData/']")));
+
+        String allTextFromMainPanel = getDriver().findElement(By.id("main-panel")).getText();
+        String[] actualText = allTextFromMainPanel.split("\n");
+
+        Assert.assertTrue(getDriver().findElements(By.xpath("//div[@id='main-panel']//tbody//tr")).isEmpty());
+        Assert.assertEquals(actualText[actualText.length - 1], expectedText);
     }
 }
