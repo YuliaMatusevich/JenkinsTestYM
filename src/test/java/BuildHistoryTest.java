fb23091d57fd28997dcc748a4f3d93f83ca9cebd
Veranika Malazhavaya <77335777+veronmolosh@users.noreply.github.com>
2022-11-28 22:43:55 -0500

    AT 08.03.01 | Build history > icon S, M, L > Verify default icon size (#797)
    * add testVerifyDefaultIconSize
    
    * add variables
    
    * refactor method

--- src/test/java/BuildHistoryTest.java
+++ src/test/java/BuildHistoryTest.java
@@ -2,8 +2,12 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+import runner.TestUtils;
 
 public class BuildHistoryTest extends BaseTest {
+
+    private static final By BUILD_NOW_BTN = By.xpath("//body[1]/div[3]/div[1]/div[1]/div[5]/span[1]");
+    private static final By ICON_SIZE = By.xpath("//a[@class='jenkins-table__button']//*[name()='svg']");
 
 
     @Test
@@ -15,5 +19,36 @@ public class BuildHistoryTest extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(By.cssSelector("#description-link")).getText(),
                 "Add description");
+    }
+
+    @Test
+    public void testVerifyDefaultIconSize() {
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(TestUtils.getRandomStr(8));
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+        getDriver().findElement(BUILD_NOW_BTN).click();
+        getDriver().findElement(By.xpath("//a[text()='Dashboard']")).click();
+        getDriver().findElement(By.linkText("Build History")).click();
+
+        int width = getDriver().findElement(ICON_SIZE).getSize().getWidth();
+        int height = getDriver().findElement(ICON_SIZE).getSize().getHeight();
+        String size = getDriver().findElement(By.className("jenkins-icon-size__items-item")).getText();
+
+        switch (size) {
+            case "S\nmall":
+                Assert.assertEquals(width, 16);
+                Assert.assertEquals(height, 16);
+                break;
+            case "M\nedium":
+                Assert.assertEquals(width, 20);
+                Assert.assertEquals(height, 20);
+                break;
+            case "L\narge":
+                Assert.assertEquals(width, 24);
+                Assert.assertEquals(height, 24);
+                break;
+        }
     }
 }
