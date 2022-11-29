1aa9ab45c7620f0ce13d584ff91aaafa6d90c4dc
Stanislaw1349 <92247013+Stanislaw1349@users.noreply.github.com>
2022-11-29 23:21:44 +0300

    AT 03.02.01.02 | Multiconfiguration Project > Disable Project > Check icon Job name page (#792)
    * add testDisableMultiMultiConfigurationProjectCheckIconProjectName
    AT 03.02.01.02 | Multiconfiguration Project > Disable Project > Check icon Job name page

--- src/test/java/MulticonfigurationProjectTest.java
+++ src/test/java/MulticonfigurationProjectTest.java
@@ -253,13 +253,13 @@ public class MulticonfigurationProjectTest extends BaseTest {
         getDriver().findElement(By.id("yui-gen1-button")).click();
 
         Assert.assertEquals(getDriver().findElement(By.id
-                ("main-panel")).getText(),"Error\n‘&amp;’ is an unsafe character");
+                ("main-panel")).getText(), "Error\n‘&amp;’ is an unsafe character");
 
         deleteNewMCProject("MC Project");
     }
 
     @Test(dependsOnMethods = "testMultiConfigurationProjectBuild")
-    public void testMultiConfigurationProjectsRunJobInBuildHistory(){
+    public void testMultiConfigurationProjectsRunJobInBuildHistory() {
 
         getDriver().findElement(DASHBOARD).click();
         getDriver().findElement(By.xpath("//a[@href='/me/my-views']")).click();
@@ -271,11 +271,26 @@ public class MulticonfigurationProjectTest extends BaseTest {
         }
 
         Assert.assertEquals(getDriver().findElement(
-                By.xpath("//a[@href='/job/" + NEW_PROJECT_NAME + "/default/']")).getText(),
+                        By.xpath("//a[@href='/job/" + NEW_PROJECT_NAME + "/default/']")).getText(),
                 NEW_PROJECT_NAME + " » default");
         Assert.assertEquals(getDriver().findElement(
                         By.xpath("//a[@href='/job/" + NEW_PROJECT_NAME + "/']")).getText(),
                 NEW_PROJECT_NAME);
+    }
+    @Test
+    public void testDisableMultiMultiConfigurationProjectCheckIconProjectName() {
+        getDriver().findElement(NEW_ITEM).click();
+        getDriver().findElement(INPUT_NAME).sendKeys(PROJECT_NAME);
+        getDriver().findElement(By.xpath("//span[contains(text(), 'Multi-configuration project')]")).click();
+        getDriver().findElement(OK_BUTTON).click();
+        getDriver().findElement(SAVE_BUTTON).click();
+        getDriver().findElement(DASHBOARD).click();
+        getDriver().findElement(By.xpath(String.format("//span[contains(text(),'%s')]", PROJECT_NAME))).click();
+        getDriver().findElement(DISABLE_PROJECT).click();
+
+        Assert.assertTrue(getDriver().findElement(
+                        By.xpath("//span[@class='build-status-icon__wrapper icon-disabled icon-md']"))
+                .isDisplayed());
     }
 }
 
