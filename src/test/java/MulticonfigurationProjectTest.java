f4ad8e35606f387d886eb824109e6b5ded62c364
Stanislaw1349 <92247013+Stanislaw1349@users.noreply.github.com>
2022-12-02 06:43:19 +0300

    AT 03.02.02.02 | Multiconfiguration Project > Enable Project > Check icon Dashboard page (#869)
    * testEnableMultiMultiConfigurationProjectCheckIconDashboardPage
    
    * minor changes
    
    * minor changes2
    
    * minor changes2
    
    * minor changes2
    
    * minor changes2

--- src/test/java/MulticonfigurationProjectTest.java
+++ src/test/java/MulticonfigurationProjectTest.java
@@ -227,7 +227,7 @@ public class MulticonfigurationProjectTest extends BaseTest {
     }
 
     @Test
-    public void testDisableMultiMultiConfigurationProjectCheckIconDashboardPage() {
+    public void testDisableMultiConfigurationProjectCheckIconDashboardPage() {
         getDriver().findElement(NEW_ITEM).click();
         getDriver().findElement(INPUT_NAME).sendKeys(PROJECT_NAME);
         getDriver().findElement(By.xpath("//span[contains(text(), 'Multi-configuration project')]")).click();
@@ -237,8 +237,19 @@ public class MulticonfigurationProjectTest extends BaseTest {
         getDriver().findElement(By.xpath(String.format("//span[contains(text(),'%s')]", PROJECT_NAME))).click();
         getDriver().findElement(DISABLE_PROJECT).click();
         getDriver().findElement(DASHBOARD).click();
+
         Assert.assertTrue(getDriver().findElement((By.xpath(
                         String.format("//tr[@id='job_%s']//span[@class='build-status-icon__wrapper icon-disabled icon-md']", PROJECT_NAME))))
+                .isDisplayed());
+    }
+
+    @Test(dependsOnMethods = "testDisableMultiConfigurationProjectCheckIconDashboardPage")
+    public void testEnableMultiConfigurationProjectCheckIconDashboardPage() {
+        getDriver().findElement(By.xpath(String.format("//span[contains(text(),'%s')]", PROJECT_NAME))).click();
+        getDriver().findElement(ENABLE_PROJECT_BUTTON).click();
+        getDriver().findElement(DASHBOARD).click();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span/span/*[name()='svg' and @tooltip='Not built']"))
                 .isDisplayed());
     }
 
@@ -282,7 +293,7 @@ public class MulticonfigurationProjectTest extends BaseTest {
     }
 
     @Test
-    public void testDisableMultiMultiConfigurationProjectCheckIconProjectName() {
+    public void testDisableMultiConfigurationProjectCheckIconProjectName() {
         getDriver().findElement(NEW_ITEM).click();
         getDriver().findElement(INPUT_NAME).sendKeys(PROJECT_NAME);
         getDriver().findElement(By.xpath("//span[contains(text(), 'Multi-configuration project')]")).click();
@@ -325,8 +336,8 @@ public class MulticonfigurationProjectTest extends BaseTest {
                 "Disable Project");
     }
 
-    @Test(dependsOnMethods = "testDisableMultiMultiConfigurationProjectCheckIconProjectName")
-    public void testEnableMultiMultiConfigurationProjectCheckIconProjectName() {
+    @Test(dependsOnMethods = "testDisableMultiConfigurationProjectCheckIconProjectName")
+    public void testEnableMultiConfigurationProjectCheckIconProjectName() {
         getDriver().findElement(By.xpath(String.format("//span[contains(text(),'%s')]", PROJECT_NAME))).click();
         getDriver().findElement(ENABLE_PROJECT_BUTTON).click();
 
