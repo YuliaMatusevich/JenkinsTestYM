862f93f498510242b5a235492cdf516fb50770a5
Evan Mai <mai.evan@yahoo.com>
2022-12-07 23:50:50 -0800

    ERR | PeoplePageTest (#942)
     refactoring  add getWait() for list

--- src/test/java/PeoplePageTest.java
+++ src/test/java/PeoplePageTest.java
@@ -1,9 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -12,7 +10,8 @@ import java.util.stream.Collectors;
 
 public class PeoplePageTest extends BaseTest {
 
-    private static final String USER_NAME = runner.TestUtils.getRandomStr();
+
+    private static final String user_name = runner.TestUtils.getRandomStr();
 
     @Test
     public void testPeoplePage() {
@@ -36,30 +35,28 @@ public class PeoplePageTest extends BaseTest {
         }
     }
 
-    @Ignore
     @Test
     public void testFindUserInThePeopleSection() {
         getDriver().findElement(By.cssSelector("a[href='/manage']")).click();
         getDriver().findElement(By.cssSelector("a[href='securityRealm/']")).click();
         getDriver().findElement(By.cssSelector("a[href='addUser']")).click();
 
-        getDriver().findElement(By.id("username")).sendKeys(USER_NAME);
+        getDriver().findElement(By.id("username")).sendKeys(user_name);
         getDriver().findElement(By.name("password1")).sendKeys("1234567890");
         getDriver().findElement(By.name("password2")).sendKeys("1234567890");
-        getDriver().findElement(By.name("fullname")).sendKeys(USER_NAME);
+        getDriver().findElement(By.name("fullname")).sendKeys(user_name);
         getDriver().findElement(By.name("email")).sendKeys("email@email.com");
         getDriver().findElement(By.id("yui-gen1-button")).click();
 
         getDriver().findElement(By.cssSelector("a[href='/'] ")).click();
         getDriver().findElement(By.cssSelector("a[href='/asynchPeople/'] ")).click();
 
-        List<WebElement> list = getDriver().findElements(By.className("jenkins-table__link"));
+        List<WebElement> list = getWait(1).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("jenkins-table__link")));
         List<String> lst = list.stream().map(WebElement::getText).collect(Collectors.toList());
 
-        Assert.assertTrue(lst.contains(USER_NAME));
+        Assert.assertTrue(lst.contains(user_name));
     }
 
-    @Ignore
     @Test(dependsOnMethods = "testFindUserInThePeopleSection")
     public void testPeopleDeleteUser() {
         getDriver().findElement(By.cssSelector("a[href='/'] ")).click();
@@ -67,13 +64,13 @@ public class PeoplePageTest extends BaseTest {
         getDriver().findElement(By.cssSelector("a[href='/manage']")).click();
         getDriver().findElement(By.cssSelector("a[href='securityRealm/']")).click();
 
-        getWait(3).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href='user/" + USER_NAME.toLowerCase()  + "/delete']"))).click();
+        getWait(3).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href='user/" + user_name.toLowerCase()  + "/delete']"))).click();
 
         getDriver().findElement(By.id("yui-gen1-button")).click();
 
-        List<WebElement> list = getDriver().findElements(By.className("jenkins-table__link"));
+        List<WebElement> list = getWait(1).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("jenkins-table__link")));
         List<String> lst = list.stream().map(WebElement::getText).collect(Collectors.toList());
 
-        Assert.assertFalse(lst.contains(USER_NAME));
+        Assert.assertFalse(lst.contains(user_name));
     }
 }
