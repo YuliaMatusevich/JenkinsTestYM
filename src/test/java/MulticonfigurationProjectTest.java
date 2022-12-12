ee7489db4058fd4a3f18c4d434e15b6c72b80716
UmidaKakharova <umida1904umida@gmail.com>
2022-12-12 14:48:13 -0500

    RF_disableMCProject

--- src/test/java/MulticonfigurationProjectTest.java
+++ src/test/java/MulticonfigurationProjectTest.java
@@ -346,7 +346,6 @@ public class MulticonfigurationProjectTest extends BaseTest {
 
     @Test(dependsOnMethods = "testCreateMultiConfigurationProjectWithValidName")
     public void testDisableMultiConfigurationProject() {
-
         Boolean projectIconText = new HomePage(getDriver())
                 .clickProject(PROJECT_NAME)
                 .clickDisableButton()
