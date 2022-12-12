3f85e2bf9f29444ec523bfc06d84c0628199f9b5
Ilya Cheshkov <0921408@gmail.com>
2022-12-12 22:13:14 +0100

    RF | BuildHistoryTest > metods (#1056)
    * Removed unusable methods, rewrote tests
    
    * Update BuildHistoryTest.java
    
    * Update BuildHistoryTest.java

--- src/test/java/BuildHistoryTest.java
+++ src/test/java/BuildHistoryTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -17,27 +18,20 @@ public class BuildHistoryTest extends BaseTest {
     private static final By DASHBOARD = By.xpath("//a[contains(text(), 'Dashboard')]");
     private static final By BUILD_HISTORY = By.linkText("Build History");
 
-    private static String jobName = "";
+    private String jobName;
 
-    private void createProject() {
+    private void createProject(String description) {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(TestUtils.getRandomStr(8));
+        jobName = TestUtils.getRandomStr(8);
+        getDriver().findElement(By.id("name")).sendKeys(jobName);
         getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
         getDriver().findElement(By.id("ok-button")).click();
+        if (!(description.equals("empty"))) {
+            getDriver().findElement(By.name("description")).sendKeys(description);
+        }
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
     }
 
-    public List<WebElement> getListOfElements(By by) {
-
-        return getDriver().findElements(by);
-    }
-
-    public int getListSize(By by) {
-
-        return getListOfElements(by).size();
-    }
-
-    @Ignore
     @Test
     public void testVerifyDefaultIconSize() {
         getDriver().findElement(By.linkText("New Item")).click();
@@ -71,7 +65,7 @@ public class BuildHistoryTest extends BaseTest {
 
     @Test
     public void testH1Header_BuildHistory() {
-        createProject();
+        createProject("empty");
         getDriver().findElement(DASHBOARD).click();
         getDriver().findElement(BUILD_HISTORY).click();
 
@@ -81,7 +75,7 @@ public class BuildHistoryTest extends BaseTest {
 
     @Test
     public void testValidityCreateBuildOnPage() {
-        createProject();
+        createProject("empty");
         getDriver().findElement(By.linkText("Build Now")).click();
         getDriver().findElement(By.xpath("//div[@class='jenkins-pane__header--build-history']/a")).click();
 
@@ -117,22 +111,19 @@ public class BuildHistoryTest extends BaseTest {
     public void testNumberOfColumns_ProjectStatusTable() {
         getDriver().findElement(BUILD_HISTORY).click();
 
-        Assert.assertEquals(getListSize(By.xpath("//table[@id='projectStatus']/thead/tr/th")), 5);
+        Assert.assertEquals(getDriver().findElements(By.xpath("//table[@id='projectStatus']/thead/tr/th")).size(), 5);
     }
-
+    
     @Ignore
     @Test
     public void testTimelineItemExist() {
-        createProject();
+        createProject("empty");
         getDriver().findElement(By.linkText("Build Now")).click();
         getDriver().findElement(DASHBOARD).click();
+        String jobLink = getDriver().getCurrentUrl() + "job/" + jobName + "/1/";
         getDriver().findElement(BUILD_HISTORY).click();
-
-        while (!((getDriver().findElement(By.xpath("//table[@id='projectStatus']//td[4]")).getText().equals("stable")))) {
-            getDriver().navigate().refresh();
-        }
+        getWait(10).until(ExpectedConditions.not(ExpectedConditions.textToBe(By.xpath("//table[@id='projectStatus']//td[4]"), "stable")));
         getDriver().findElement(By.xpath("//div[contains(text(), \"" + jobName + "\")]")).click();
-        String jobLink = "http://localhost:8080/job/" + jobName + "/1/";
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class='timeline-event-bubble-title']/a")).getAttribute("href"), jobLink);
     }
@@ -140,6 +131,9 @@ public class BuildHistoryTest extends BaseTest {
     @Ignore
     @Test(dependsOnMethods = "testTimelineItemExist")
     public void testDescriptionIsAdded() {
+        getDriver().navigate().refresh();
+        getDriver().navigate().refresh();
+        getDriver().navigate().refresh();
         getDriver().findElement(By.xpath("//table/tbody/tr/td/a[@class='jenkins-table__link jenkins-table__badge model-link inside']/button")).click();
         getDriver().findElement(By.xpath("//span[contains(text(),'Edit Build Information')]")).click();
         getDriver().findElement(By.name("description")).sendKeys(TestUtils.getRandomStr(8));
@@ -154,9 +148,7 @@ public class BuildHistoryTest extends BaseTest {
         getDriver().findElement(DASHBOARD).click();
         getDriver().findElement(BUILD_HISTORY).click();
         getDriver().findElement(By.xpath("//table/tbody/tr/td/a[@class='jenkins-table__link jenkins-table__badge model-link inside']/button")).click();
-
         scrollToElement(getDriver(), getDriver().findElement(By.xpath("//span[contains(text(),'Delete build')]")));
-
         getDriver().findElement(By.xpath("//span[contains(text(),'Delete build')]")).click();
         getDriver().findElement(By.xpath("//button[@id='yui-gen1-button']")).click();
 
@@ -166,7 +158,6 @@ public class BuildHistoryTest extends BaseTest {
     @Test
     public void testRedirectToMainPage() {
         HomePage homePage = new HomePage(getDriver())
-
                 .clickBuildHistory()
                 .clickDashboard();
 
