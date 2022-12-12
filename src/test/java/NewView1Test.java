4a5f26fd09b6ce6a4da6095453baf8fac82726cd
Vadim Trefilov <90708778+VadimTref@users.noreply.github.com>
2022-12-12 04:50:44 -0500

    RF | NewView1Test > testDeleteAllViews (#1043)

--- src/test/java/NewView1Test.java
+++ src/test/java/NewView1Test.java
@@ -2,32 +2,16 @@ import model.EditViewPage;
 import model.HomePage;
 import model.MyViewsPage;
 import model.ViewPage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class NewView1Test extends BaseTest {
 
-    private static final By BY_DASHBOARD_LINK = By.xpath("//ul[@id='breadcrumbs']//a[@href='/']");
-    private static final By BY_MY_VIEWS = By.cssSelector("a[href='/me/my-views']");
-    private static final By BY_DELETE_VIEW = By.xpath("//a[@href='delete']");
-    private static final By BY_LIST_VIEWS =
-            By.cssSelector(".tabBar .tab a[href*='/user/admin/my-views/view/']");
     private static final String GLOBAL_VIEW_NAME = "Global_View";
     private static final String LIST_VIEW_NAME = "List_View";
     private static final String MY_VIEW_NAME = "My_View";
     private static final String LIST_VIEW_RENAME = "New_List_View";
-
-    private String getListViewsNames() {
-        StringBuilder listViewsNames = new StringBuilder();
-        for (WebElement view : getDriver().findElements(BY_LIST_VIEWS)) {
-            listViewsNames.append(view.getText()).append(" ");
-        }
-
-        return listViewsNames.toString().trim();
-    }
 
     @Test
     public void testCreateViews() {
@@ -130,14 +114,10 @@ public class NewView1Test extends BaseTest {
 
     @Test(dependsOnMethods = "testDeleteView")
     public void testDeleteAllViews() {
-        getDriver().findElement(BY_DASHBOARD_LINK).click();
-        getDriver().findElement(BY_MY_VIEWS).click();
-        for (int i = getDriver().findElements(BY_LIST_VIEWS).size() - 1; i >= 0; i--) {
-            getDriver().findElements(BY_LIST_VIEWS).get(i).click();
-            getDriver().findElement(BY_DELETE_VIEW).click();
-            getDriver().findElement(By.id("yui-gen1-button")).click();
-        }
+        MyViewsPage myViewsPage = new HomePage(getDriver())
+                .clickMyViews()
+                .deleteAllViews();
 
-        Assert.assertEquals(getListViewsNames(), "");
+        Assert.assertEquals(myViewsPage.getListViewsNames(), "");
     }
 }
--- src/test/java/model/MyViewsPage.java
+++ src/test/java/model/MyViewsPage.java
@@ -13,11 +13,17 @@ public class MyViewsPage extends HomePage{
     @FindBy(css = "a[title='New View']")
     private WebElement newView;
 
-    @FindBy(css = ".tabBar .tab a[href]")
-    private List <WebElement> listViews;
+    @FindBy(css = ".tabBar .tab a[href*='/user/admin/my-views/view/']")
+    private List<WebElement> listViews;
 
     @FindBy(css = ".pane-header-title")
     private List <WebElement> listViewActiveFilters;
+
+    @FindBy(xpath = "//a[@href='delete']")
+    private WebElement deleteViewItem;
+
+    @FindBy(id = "yui-gen1-button")
+    private WebElement yesButtonDeleteView;
 
     public MyViewsPage(WebDriver driver) {
         super(driver);
@@ -29,14 +35,9 @@ public class MyViewsPage extends HomePage{
         return new NewViewPage(getDriver());
     }
 
-    public List<WebElement> getListViews() {
-
-        return listViews;
-    }
-
     public String getListViewsNames() {
         StringBuilder listViewsNames = new StringBuilder();
-        for (WebElement view : getListViews()) {
+        for (WebElement view : listViews) {
             listViewsNames.append(view.getText()).append(" ");
         }
 
@@ -54,4 +55,13 @@ public class MyViewsPage extends HomePage{
         return new ViewPage(getDriver());
     }
 
+    public MyViewsPage deleteAllViews() {
+        for (int i = listViews.size() - 1; i >= 0; i--) {
+            listViews.get(i).click();
+            deleteViewItem.click();
+            yesButtonDeleteView.click();
+        }
+
+        return this;
+    }
 }
