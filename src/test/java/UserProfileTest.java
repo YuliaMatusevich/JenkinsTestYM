17bb412d0e34f2922f8de939ad822d14077b79dc
RomanSaf <75407306+RomanSaf@users.noreply.github.com>
2022-12-14 16:49:12 -0800

    https://trello.com/c/4fBMb0dc/1010-rf-1501002-user-profile-page-edit-description-test-to-pom (#1105)
    1.RF test [testUserProfileEditDescription] to POM
    2.Removing unused locators and empty lines

--- src/test/java/UserProfileTest.java
+++ src/test/java/UserProfileTest.java
@@ -7,33 +7,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
-
 import java.time.Duration;
 
 public class UserProfileTest extends BaseTest {
 
-    private static final By USER_ICON = By.xpath("//a[contains(@href, 'user')]");
-    private static final By ADD_DES = By.id("description-link");
-    private static final By INPUT_FIELD = By.tagName("textarea");
-    private static final By SAVE_BUTTON = By.id("yui-gen1-button");
     private static final String TEXT = RandomStringUtils.randomAlphanumeric(50);
     private static final String TEXT_EDIT = RandomStringUtils.randomAlphanumeric(10);
-
-    private WebDriverWait wait;
-
-    private WebDriverWait getWait() {
-        if (wait == null) {
-            wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
-        }
-        return wait;
-    }
-
-    public void deleteDescription() {
-        getWait().until(ExpectedConditions.elementToBeClickable(ADD_DES)).click();
-        getDriver().findElement(INPUT_FIELD).clear();
-        getDriver().findElement(SAVE_BUTTON).click();
-    }
-
+    
     @Test
     public void testUserProfileAddDescription() {
         String actualResult = new HomePage(getDriver())
@@ -62,7 +42,6 @@ public class UserProfileTest extends BaseTest {
 
     @Test
     public void testUserProfilePreviewDescription() {
-
         StatusUserPage statusUserPage = new HomePage(getDriver())
                 .clickUserIcon()
                 .clickAddDescriptionLink()
@@ -75,16 +54,14 @@ public class UserProfileTest extends BaseTest {
 
     @Test(dependsOnMethods = "testUserProfileAddDescription")
     public void testUserProfileEditDescription() {
+        String actualResult = new HomePage(getDriver())
+                .clickUserIcon()
+                .clickAddDescriptionLink()
+                .clearDescriptionInputField()
+                .inputTextInDescriptionField(TEXT_EDIT)
+                .clickSaveButton()
+                .getDescriptionText();
 
-        getDriver().findElement(USER_ICON).click();
-        getDriver().findElement(ADD_DES).click();
-        getDriver().findElement(INPUT_FIELD).clear();
-        getDriver().findElement(INPUT_FIELD).sendKeys(TEXT_EDIT);
-        getDriver().findElement(SAVE_BUTTON).click();
-
-        Assert.assertTrue(getDriver().
-                findElement(By.xpath("//div[contains(text(),'" + TEXT_EDIT + "')]")).isDisplayed());
-
-        deleteDescription();
+        Assert.assertEquals(actualResult, TEXT_EDIT);
     }
 }
