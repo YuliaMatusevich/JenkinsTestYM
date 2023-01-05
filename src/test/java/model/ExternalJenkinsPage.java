0305411f548c8ec6bf254cb18caa38c883949880
Kate Bay <99692970+cyferkiBY@users.noreply.github.com>
2023-01-05 02:31:22 +0100

    ERR | FooterComponentTest > testFooterLinkJenkinsRedirectToPage (#1243)
    Co-authored-by: Kate Bay <kate.by.2022@yandex.ru>

--- src/test/java/model/ExternalJenkinsPage.java
+++ src/test/java/model/ExternalJenkinsPage.java
@@ -6,18 +6,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ExternalJenkinsPage extends FooterComponent {
-    @FindBy(xpath = "//a[@class='navbar-brand']")
-    private WebElement textJenkins;
 
     @FindBy(xpath = "//h1")
     private WebElement textHeaderJenkins;
 
     public ExternalJenkinsPage(WebDriver driver) {
         super(driver);
-    }
-
-    public String getTextJenkins() {
-        return textJenkins.getText();
     }
 
     public String getHeaderText() {
