15d9b7984c168c7cd401cbcbec2624fcedf1364f
RadasIvan <115185867+RadasIvan@users.noreply.github.com>
2023-01-26 21:02:14 +0000

    MovedListViewTestToViewTest (#1332)

--- src/test/java/tests/ViewsTest.java
+++ src/test/java/tests/ViewsTest.java
@@ -571,4 +571,85 @@ public class ViewsTest extends BaseTest {
 
         Assert.assertFalse(myViewsPage.getListViewsNames().contains(LIST_VIEW_NAME));
     }
+
+    @Test
+    public void testCreateNewListViewWithExistingJob() {
+        final String projectOne = getRandomStr();
+
+        int quantityProjectsInListView = new HomePage(getDriver())
+                .getSideMenu()
+                .clickNewItem()
+                .setItemName(projectOne)
+                .selectFreestyleProjectAndClickOk()
+                .clickSaveButton()
+                .getBreadcrumbs()
+                .clickDashboard()
+                .clickAddViewLink()
+                .setViewName(LIST_VIEW_NAME)
+                .setListViewType()
+                .clickCreateButton(new EditListViewPage(getDriver()))
+                .addJobToView(projectOne)
+                .clickOkButton()
+                .getJobNamesList().size();
+
+        Assert.assertEquals(quantityProjectsInListView, 1);
+        Assert.assertTrue(new HomePage(getDriver()).getViewList().contains(LIST_VIEW_NAME));
+    }
+
+    @Test(dependsOnMethods = "testCreateNewListViewWithExistingJob")
+    public void testEditViewAddDescription() {
+        final String descriptionRandom = getRandomStr();
+
+        String actualDescription = new HomePage(getDriver())
+                .clickView(LIST_VIEW_NAME)
+                .clickEditListView()
+                .addDescription(descriptionRandom)
+                .clickOkButton()
+                .getTextDescription();
+
+        Assert.assertEquals(actualDescription, descriptionRandom);
+    }
+
+    @Test(dependsOnMethods = "testEditViewAddDescription")
+    public void testEditViewDeleteDescription() {
+        String descriptionText = new HomePage(getDriver())
+                .clickView(LIST_VIEW_NAME)
+                .clickEditDescription()
+                .clearDescription()
+                .clickSaveButton()
+                .getTextDescription();
+
+        Assert.assertEquals(descriptionText, "");
+    }
+
+    @Test(dependsOnMethods = "testEditViewDeleteDescription")
+    public void testRemoveSomeHeadersFromProjectStatusTableInListView() {
+        final List<String> namesRemoveColumns = List.of("Weather", "Last Failure", "Last Duration");
+
+        int numberOfJobTableHeadersListView = new HomePage(getDriver())
+                .clickView(LIST_VIEW_NAME)
+                .clickEditListView()
+                .removeSomeColumns(namesRemoveColumns)
+                .clickApplyButton()
+                .clickOkButton()
+                .getJobTableHeadersSize();
+
+        int numberOfJobTableHeadersAll = new ViewPage(getDriver())
+                .getBreadcrumbs()
+                .clickDashboard()
+                .getJobTableHeadersSize();
+
+        Assert.assertNotEquals(numberOfJobTableHeadersAll, numberOfJobTableHeadersListView);
+    }
+
+    @Test(dependsOnMethods = "testRemoveSomeHeadersFromProjectStatusTableInListView")
+    public void testDeleteListView() {
+        List<String> viewList = new HomePage(getDriver())
+                .clickView(LIST_VIEW_NAME)
+                .clickDeleteViewToHomePage()
+                .clickYes()
+                .getViewList();
+
+        Assert.assertFalse(viewList.contains(LIST_VIEW_NAME));
+    }
 }
