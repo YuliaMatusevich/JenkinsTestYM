4082c7531a545f2756ca621cca81f1a3e2f8e0d5
DenSebrovsky <den.sebrovsky@mail.ru>
2023-02-13 13:07:01 +0300

    RF | FreestyleProjectTest> testFreestyleProjectBuildDateAndTime
    remove @Ignore from testFreestyleProjectBuildDateAndTime

--- src/test/java/tests/FreestyleProjectTest.java
+++ src/test/java/tests/FreestyleProjectTest.java
@@ -4,7 +4,6 @@ import model.*;
 import model.config_pages.FreestyleProjectConfigPage;
 import model.status_pages.FreestyleProjectStatusPage;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 import runner.ProjectMethodsUtils;
@@ -308,7 +307,6 @@ public class FreestyleProjectTest extends BaseTest {
         Assert.assertTrue(selectedCheckbox);
     }
 
-    @Ignore
     @Test
     public void testFreestyleProjectBuildDateAndTime() {
         ProjectMethodsUtils.createNewFreestyleProject(getDriver(),TestDataUtils.FREESTYLE_PROJECT_NAME);
