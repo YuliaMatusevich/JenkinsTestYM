7a14d5082805ad8838c53df6c3591c1f27d18453
Stanislav Akulionok <92247013+Stanislaw1349@users.noreply.github.com>
2023-03-27 23:59:46 +0300

    add Allure annotations to the tests (#1487)

--- src/test/java/tests/MultiConfigurationProjectTest.java
+++ src/test/java/tests/MultiConfigurationProjectTest.java
@@ -19,6 +19,11 @@ import java.util.List;
 
 public class MultiConfigurationProjectTest extends BaseTest {
 
+    @TmsLink("Rj2aehGq")
+    @Owner("Vita Zharskaya")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("Function")
+    @Description("Verify that the multi-configuration project can be description can be added")
     @Test
     public void testMultiConfigurationProjectAddDescription() {
         ProjectMethodsUtils.createNewMultiConfigurationProject(getDriver(), TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME);
@@ -32,6 +37,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertEquals(multiConfPage.getDescriptionText(), "Description");
     }
 
+    @TmsLink("qKR0YdeZ")
+    @Owner("Artem Chumachenko")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description("Verify that the multi-configuration project can be renamed in the drop-down menu")
     @Test
     public void testMultiConfigurationProjectRenameProjectViaDropDownMenu() {
         ProjectMethodsUtils.createNewMultiConfigurationProject(getDriver(), TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME);
@@ -45,6 +55,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertEquals(multiConfigPrStatusPage.getNameMultiConfigProject(TestDataUtils.MULTI_CONFIGURATION_PROJECT_RENAME), TestDataUtils.MULTI_CONFIGURATION_PROJECT_RENAME);
     }
 
+    @TmsLink("l45PAlUD")
+    @Owner("Artem Chumachenko")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description("Verify that the 'MultiConfigurationProject' can be renamed in the side menu")
     @Test
     public void testMultiConfigurationProjectRenameProjectViaSideMenu() {
         ProjectMethodsUtils.createNewMultiConfigurationProject(getDriver(), TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME);
@@ -59,6 +74,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertEquals(multiConfigPrStatusPage.getNameMultiConfigProject(TestDataUtils.MULTI_CONFIGURATION_PROJECT_RENAME), TestDataUtils.MULTI_CONFIGURATION_PROJECT_RENAME);
     }
 
+    @TmsLink("0jufQC14")
+    @Owner("Zinaida Konovalova")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description("Verify that the 'MultiConfigurationProject' can be renamed to project status page")
     @Test
     public void testMultiConfigurationProjectDelete() {
         HomePage homePage = new HomePage(getDriver())
@@ -78,6 +98,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertFalse(homePage.getJobNamesList().contains(TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME));
     }
 
+    @TmsLink("tPQnmQk2")
+    @Owner("Vita Zharskaya")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("Function")
+    @Description("Verify that the 'MultiConfigurationProject' can be disabled to project configuration page")
     @Test
     public void testMultiConfigurationProjectDisable() {
         ProjectMethodsUtils.createNewMultiConfigurationProject(getDriver(), TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME);
@@ -92,6 +117,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertTrue(multiConfigurationProjectStatusPage.getTextDisabledWarning().contains("This project is currently disabled"));
     }
 
+    @TmsLink("Po4bZcIE")
+    @Owner("Vita Zharskaya")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("Function")
+    @Description("Verify that the multi-configuration project can be enabled to project configuration page")
     @Test(dependsOnMethods = "testMultiConfigurationProjectDisable")
     public void testMultiConfigurationProjectEnable() {
         MultiConfigurationProjectStatusPage multiConfigurationProjectStatusPage = new HomePage(getDriver())
@@ -104,6 +134,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertTrue(multiConfigurationProjectStatusPage.disableButtonIsDisplayed());
     }
 
+    @TmsLink("ki8fvOza")
+    @Owner("Arailym Ashimova")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("Function")
+    @Description("Verify that the 'MultiConfigurationProject' I can make to build multi-configuration projects, so that I can build them manually")
     @Test
     public void testMultiConfigurationProjectBuild() {
         ProjectMethodsUtils.createNewMultiConfigurationProject(getDriver(), TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME);
@@ -125,6 +160,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertNotEquals(countBuildsAfterNewBuild, countBuildsBeforeNewBuild);
     }
 
+    @TmsLink("im1BdS7m")
+    @Owner("Natali Chervona")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("Function")
+    @Description("Verify that the so that can find a previously created multi-configuration project")
     @Test
     public void testFindMultiConfigurationProject() {
         ProjectMethodsUtils.createNewMultiConfigurationProject(getDriver(), TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME);
@@ -136,6 +176,7 @@ public class MultiConfigurationProjectTest extends BaseTest {
     }
 
     @TmsLink("2r92DxXC")
+    @Owner("Stanislaw Aklionak")
     @Severity(SeverityLevel.NORMAL)
     @Feature("Function")
     @Description("Check if multi-configuration project can be disabled")
@@ -178,6 +219,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertEquals(jobStatusIconTooltip, "Not built");
     }
 
+    @TmsLink("xUfTdt7U")
+    @Owner("Elizaveta Skobeleva")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description("Verify that the multi-configuration project can be renamed to side menu")
     @Test
     public void testMultiConfigurationProjectRenameToInvalidNameViaSideMenu() {
         RenameItemErrorPage renameItemErrorPage = new HomePage(getDriver())
@@ -198,6 +244,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertEquals(renameItemErrorPage.getErrorMessage(), "‘&amp;’ is an unsafe character");
     }
 
+    @TmsLink("rFqOXdzk")
+    @Owner("Arailym Ashimova")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description()
     @Test(dependsOnMethods = {"testMultiConfigurationProjectBuild"})
     public void testMultiConfigurationProjectsRunJobInBuildHistory() {
         List<String> listNameOfLabels = new HomePage(getDriver())
@@ -211,6 +262,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertTrue(listNameOfLabels.contains(TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME + " #1"));
     }
 
+    @TmsLink("Bs5nMG6n")
+    @Owner("Stanislaw Aklionak")
+    @Severity(SeverityLevel.TRIVIAL)
+    @Feature("User Interface")
+    @Description("Verify that when you turn off the project, the project icon changes its status to 'disable' on the project configuration page")
     @Test
     public void testMultiConfigurationProjectDisableCheckIconProjectName() {
         ProjectMethodsUtils.createNewMultiConfigurationProject(getDriver(), TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME);
@@ -221,6 +277,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertTrue(multiConfigPrStatusPage.iconProjectDisabledIsDisplayed());
     }
 
+    @TmsLink("WdKwAoJS")
+    @Owner("Stanislaw Aklionak")
+    @Severity(SeverityLevel.TRIVIAL)
+    @Feature("User Interface")
+    @Description("Verify that when you turn on the project, the project icon changes its status to 'enable' on the project configuration page")
     @Test(dependsOnMethods = {"testMultiConfigurationProjectDisableCheckIconProjectName"})
     public void testMultiConfigurationProjectEnableCheckIconProjectName() {
         MultiConfigurationProjectStatusPage multiConfigPrStatusPage = new HomePage(getDriver())
@@ -230,6 +291,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertTrue(multiConfigPrStatusPage.iconProjectEnabledIsDisplayed());
     }
 
+    @TmsLink("Mob6yuVf")
+    @Owner("Umida Kakharova")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description("Verify that multi-configuration project can be enabled on dashboard")
     @Test(dependsOnMethods = "testMultiConfigurationProjectDisableCheckIconDashboardPage")
     public void testEnableMultiConfigurationProject() {
         HomePage buildNowButton = new HomePage(getDriver())
@@ -242,6 +308,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertTrue(buildNowButton.buildNowButtonIsDisplayed());
     }
 
+    @TmsLink("sTZpY5Hc")
+    @Owner("Dmitry Starski")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description("Verify that the assembly step verifies the success of the assembly")
     @Flaky
     @Test
     public void testMultiConfigurationProjectWithBuildStepCheckBuildSuccess() {
@@ -272,6 +343,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertTrue(multiConfigProjectConsole.getConsoleOutputText().contains("Finished: SUCCESS"));
     }
 
+    @TmsLink("tEFPxZt4")
+    @Owner("Nadia Ludanik")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description("Verify that set build can steps and successfully build the project")
     @Test
     public void testNewestBuildsButton() {
         MultiConfigurationProjectStatusPage mcpStatusPage = new HomePage(getDriver())
@@ -288,6 +364,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertTrue(mcpStatusPage.NewestBuildIsDisplayed());
     }
 
+    @TmsLink("zdWbAye5")
+    @Owner("Arailym Ashimova")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description("Verify that can there wil be create configuration matrix with non build stages")
     @Test
     public void testSetConfigurationMatrix() {
         ProjectMethodsUtils.createNewMultiConfigurationProject(getDriver(), TestDataUtils.MULTI_CONFIGURATION_PROJECT_NAME);
@@ -310,6 +391,11 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertTrue(configMatrix.configurationMatrixIsDisplayed());
     }
 
+    @TmsLink("x19sKw4m")
+    @Owner("Kate Bay")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Function")
+    @Description("Verify that can  set content in three build steps and successfully saved")
     @Test
     public void testSetContentInThreeBuildStepsBuildStatusOnGitHubCommitSaved() {
         final int COUNT_BUILD_STEPS = 3;
